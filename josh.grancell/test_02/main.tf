929de349a073a1388bc60161e623f89db202e38f
Josh Grancell <josh.grancell@bottomline.com>
2020-05-18 09:45:06 -0500

    Added second test group

---
+++ josh.grancell/test_02/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "http" {}
+}
+
+module "test_001" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vljgtest101"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = "4"
+  memory               = "4096"
+  foreman_environment  = "master"
+  foreman_hostgroup    = "BT Base Server"
+  lob                  = "CEA"
+}
+
+output "test_servers" {
+  value = {
+    "${module.test_001.fqdn}" = "${module.test_001.ip}"
+  }
+}
