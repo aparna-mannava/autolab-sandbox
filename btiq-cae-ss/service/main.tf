894694eb7d450f570fb4b81b73fb38067f7bf5d1
SP <sp@banksight.com>
2020-06-23 02:00:20 +0530

    Adding streamset changes in main.tf to change app reference

--- btiq-cae-ss/service/main.tf
+++ btiq-cae-ss/service/main.tf
@@ -3,8 +3,7 @@ terraform {
 }
 module "streamset-service" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlcaesset01.auto.saas-n.com"
-  bt_infra_environment = "ny2-autolab-app"  
+  hostname             = "us01vlcaesset01.auto.saas-n.com" 
   bt_infra_cluster     = "ny2-aza-vmw-autolab"
   bt_infra_network     = "ny2-autolab-app"
   os_version           = "rhel7"
