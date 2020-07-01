5a416a603b8fd197064df9f7898c5adba7884518
laura.labranche <laura.labranche@bottomline.com>
2020-07-01 12:55:51 -0400

    change of build

--- llabranche/mytest00/main.tf
+++ llabranche/mytest00/main.tf
@@ -1,22 +1,41 @@
 terraform {
   backend "http" {}
 }
+
+locals {
+  product        = "autolab"
+  environment    = "nonprod"
+  datacenter     = "ny2"
+  hostname       = "us01vwmytstl0003"
+  hostgroup      = "BT MSSQL 2016 Server"
+  facts          = {
+    "bt_tier"         = "dev"
+    "bt_bfs_timezone" = "Eastern Standard Time"
+  }
+}
+
+
 module "mytest_wins_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vwmytstl0003"
-  bt_infra_environment = "ny2-autolab-app"
-  os_version           = "win2016"
-  foreman_environment  = "nonprod"
-  foreman_hostgroup    = "BT MSSQL 2016 Server"
-  datacenter           = "ny2"
+  hostname             = "${local.hostname}"
+  alias                = ""
+  bt_infra_cluster     = "ny2-aza-vmw-autolab"
+  bt_infra_network     = "ny2-autolab-db"
   lob                  = "cloud"
+  os_version           = "win2016"
+  cpus                 = "2"
+  memory               = "4096"
+  external_facts       = "${local.facts}"
+  foreman_environment  = "${local.environment}"
+  foreman_hostgroup    = "${local.hostgroup}"
+  datacenter           = "${local.datacenter}"
   additional_disks     = {
     1 = "200",
-    2 = "200"
-  }
-  external_facts       = {
-    "bt_tier" = "dev"
-    "bt_bfs_timezone" = "Eastern Standard Time"
+    2 = "100",
+    3 = "100",
+    4 = "100",
+    5 = "100",
+    6 = "100"
   }
 }
 
