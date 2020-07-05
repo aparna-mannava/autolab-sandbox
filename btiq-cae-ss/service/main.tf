0d144afec6312426937971bd70f2f3150bb748eb
SP <sp@banksight.com>
2020-07-05 23:30:11 +0530

    BTIQ-CAE 194 - Adding hostgroup and details for streamsets  and attaching the roles.

--- btiq-cae-ss/service/main.tf
+++ btiq-cae-ss/service/main.tf
@@ -1,26 +1,29 @@
 terraform {
   backend "http" {}
 }
+locals {
+  facts       = {
+    "bt_tier"    = "dev"
+    "bt_product" = "btiq_cae"
+    "bt_role"    = "streamset"
+  }
+}
 module "streamset-service" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "us01vlcaesset01" 
   bt_infra_cluster     = "ny2-aza-vmw-autolab"
   bt_infra_network     = "ny2-autolab-app"
   os_version           = "rhel7"
-  foreman_environment  = "master"
-  foreman_hostgroup    = "BT Base Server"
+  foreman_environment  = "feature_btiq_cae_ss"
+  foreman_hostgroup    = "BTIQ CAE Streamsets"
   datacenter           = "ny2"
-  lob                  = "btiq-cae"
+  lob                  = "btiq_cae"
   cpus                 = "2"
   memory        	   = "2048"
   additional_disks     = {
     1 = "20"
   }
-  external_facts       = {
-    "bt_tier" = "dev",
-    "bt_product"       = "btiq_cae"
-    "bt_role"          = "streamset"
- }
+  external_facts       = local.facts
 }
 
 output "streamset-service" {
