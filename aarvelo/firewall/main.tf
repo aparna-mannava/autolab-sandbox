d86d72e7311da52c9fd4aea02292a2e462cf9e4a
Alex Arvelo <aarvelo@bottomline.com>
2020-07-08 10:29:01 -0400

    Testing windows firewall server

---
+++ aarvelo/firewall/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "http" {}
+}
+
+locals {
+  facts       = {
+    "bt_product" = "inf"
+  }
+}
+
+module "winfw_server_1" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vwfw01"
+  bt_infra_cluster     = "ny2-aza-vmw-autolab"
+  bt_infra_network     = "ny2-autolab-app"
+  lob                  = "INF"
+  cpus                 = "2"
+  memory               = "4096"
+  external_facts       = local.facts
+  os_version           = "win2016"
+  foreman_environment  = "master"
+  foreman_hostgroup    = "BT Base Windows Server"
+  datacenter           = "ny2"
+  additional_disks     = {
+    1 = "100",
+  }
+}
+
+output "winfw_server_1" {
+  value = {
+    "fqdn"  = "${module.winfw_server_1.fqdn}",
+    "alias" = "${module.winfw_server_1.alias}",
+    "ip"    = "${module.winfw_server_1.ip}",
+  }
+}
