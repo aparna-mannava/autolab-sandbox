f28b9595960f46cb08cd4addc72956ff7dce2b21
Delanoe, Yann <ydelanoe@bottomline.com>
2020-07-15 09:52:10 +0200

    FMDO-1740 : add snapshot node to test rsync

--- ydelanoe/essnap/main.tf
+++ ydelanoe/essnap/main.tf
@@ -50,3 +50,35 @@ output "es_snap" {
     "ip"    = "${module.es_snap.ip}",
   }
 }
+
+module "es_snap2" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vlfmessnap2"
+  alias                = "fm-${local.facts.bt_loc}-${local.facts.bt_tier}-${local.facts.bt_role}2"
+  bt_infra_cluster     = local.infra_cluster
+  bt_infra_network     = local.infra_network
+  os_version           = local.image
+  foreman_environment  = local.environment
+  foreman_hostgroup    = local.hostgroup
+  datacenter           = local.datacenter
+  external_facts       = "${merge(
+    local.facts,
+    map(
+      "bt_env", "02",
+      )
+  )}"
+  cpus                 = "4"
+  memory               = "6144"
+  additional_disks     = {
+    1 = "150"
+  }
+  lob                  = local.facts.bt_product
+}
+
+output "es_snap2" {
+  value = {
+    "fqdn"  = "${module.es_snap.fqdn}",
+    "alias" = "${module.es_snap.alias}",
+    "ip"    = "${module.es_snap.ip}",
+  }
+}
