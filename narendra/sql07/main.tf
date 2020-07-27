56933f9e37d6ef2e2ff0fd8f6a589783853c363a
Chowdary <narendra.chowdary@bottomline.com>
2020-07-27 03:53:26 -0400

    create server

--- narendra/sql07/main.tf
+++ narendra/sql07/main.tf
@@ -20,23 +20,23 @@ module "inf_windns_n7" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "${local.hostname}"
   alias                = ""
-  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_cluster     = "ny2-azb-ntnx-08"
   bt_infra_network     = "ny2-autolab-db-ahv"
   lob                  = "inf"
   os_version           = "win2016"
   cpus                 = "2"
-  memory               = "1024"
+  memory               = "4096"
   external_facts       = "${local.facts}"
   foreman_environment  = "${local.environment}"
   foreman_hostgroup    = "${local.hostgroup}"
   datacenter           = "${local.datacenter}"
   additional_disks     = {
-    1 = "50",
-    2 = "50",
-    3 = "50",
-    4 = "50",
-    5 = "50",
-    6 = "50"
+    1 = "200",
+    2 = "100",
+    3 = "100",
+    4 = "100",
+    5 = "100",
+    6 = "100"
   }
 }
 
