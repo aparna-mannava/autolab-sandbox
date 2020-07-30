4c411eb20ab592864b571f282e5e9a289fae2f73
padmanabhan.s <Padmanabhan.S@bottomline.com>
2020-07-30 13:20:43 +0530

    VM creation for testing

--- btiq-cae-ss/service/main.tf
+++ btiq-cae-ss/service/main.tf
@@ -4,18 +4,18 @@ terraform {
 locals {
   facts       = {
     "bt_tier"    = "dev"
-    "bt_product" = "btiq"
+    "bt_product" = "cae"
     "bt_env"    = "1"
   }
 }
 module "streamset-service" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlcaesset03" 
+  hostname             = "us01vlbtiqset05" 
   bt_infra_network     = "ny2-autolab-db"
   bt_infra_cluster     = "ny2-aza-vmw-autolab"
   os_version           = "rhel7"
-  foreman_environment  = "master"
-  foreman_hostgroup    = "BT Streamsets Server"
+  foreman_environment  = "feature_btiq_886"
+  foreman_hostgroup    = "BTIQ CAE Streamsets"
   datacenter           = "ny2"
   lob                  = "dev"
   cpus                 = "2"
