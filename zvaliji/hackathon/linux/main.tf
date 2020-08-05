237740b5a95daacad3bbc43085e8acaa7b5d7c4e
ZAINAB VALIJI <zvaliji@bottomline.com>
2020-08-05 11:59:32 +0100

    Commit to feature/FMLDO-12511

---
+++ zvaliji/hackathon/linux/main.tf
@@ -0,0 +1,30 @@
+#comment
+terraform {
+  backend "http" {}
+}
+
+module "example_module" {
+  source           = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname         = "gb01vlexpl12511"
+  alias            = "fml-ny2-example01"
+  bt_infra_cluster = "ny2-azd-ntnx-10"
+  bt_infra_network = "ny2-autolab-app-ahv"
+  os_version       = "rhel8"
+  cpus             = "2"
+  memory           = "2048"
+  external_facts = {
+    "bt_product" = "fml"
+  }
+  foreman_environment = "master"
+  foreman_hostgroup   = "BT Base Server"
+  datacenter          = "ny2"
+  lob                 = "fml"
+}
+
+output "example_module" {
+  value = {
+    "fqdn"  = module.example_module.fqdn,
+    "alias" = module.example_module.alias,
+    "ip"    = module.example_module.ip,
+  }
+}
