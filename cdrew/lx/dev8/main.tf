c8f0c29d155038f422deafdaaffa6d68db6ad701
Drew, Chris <cdrew@bottomline.com>
2020-08-08 09:38:24 -0400

    ping for lx test build

---
+++ cdrew/lx/dev8/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  backend "s3" {}
+}
+locals {
+  facts       = {
+    "bt_tier"    = "dev"
+    "bt_product" = "lx"
+    "bt_role" = "postgresql"
+    "bt_env"    = "8"
+  }
+}
+
+module "pg-service" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vllxdb11" 
+  alias                = "lx-tst-key" 
+  bt_infra_cluster     = "ny2-azd-ntnx-10"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  foreman_environment  = "master"
+  foreman_hostgroup    = "BT LX Keycloak PG standalone"
+  datacenter           = "ny2"
+  lob                  = "dev"
+  cpus                 = "4"
+  memory               = "4098"
+  additional_disks     = {
+    1 = "100"
+    2 = "200"
+  }
+  external_facts       = local.facts
+}
+
+output "pg-service" {
+  value = {
+    "fqdn"  = "${module.pg-service.fqdn}",
+    "alias" = "${module.pg-service.alias}",
+    "ip"    = "${module.pg-service.ip}",
+  }
+  
+}
