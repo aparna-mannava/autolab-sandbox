30731547b5c60a422c945185afdef002077ddb7b
Bobby Varghese <null>
2020-08-17 12:37:18 -0400

    change network

--- bvarghese/test/main.tf
+++ bvarghese/test/main.tf
@@ -20,7 +20,7 @@ module "oradb_server_1" {
   hostname             = "us01vltestdev01"
   alias                = "${local.product}-${local.facts.bt_tier}${local.facts.bt_env}-dev01"
   bt_infra_cluster     = "ny2-azd-ntnx-10"
-  bt_infra_network     = "ny2-autolab-app-ahv"
+  bt_infra_network     = "ny2-autolab-db-ahv"
   os_version           = "rhel7"
   cpus                 = "2"
   memory               = "8192"
@@ -34,7 +34,7 @@ module "oradb_server_1" {
     2 = "200",
     3 = "50",
     4 = "50",
-	5 = "50"
+	  5 = "50"
   }
 }
 
