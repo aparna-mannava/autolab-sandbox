e402445f331c1b5374f1a5260934d4159fd998d8
Brajesh.Kumar <Brajesh.Kumar@bottomline.com>
2020-09-04 21:35:02 +0530

    Updated

--- bkumar/vm/main.tf
+++ bkumar/vm/main.tf
@@ -1,29 +1,45 @@
+#
+# Building VMs for Testing
+
 terraform {
   backend "http" {}
 }
 
-module "app_server_1" {
-  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlapp0051"
-  bt_infra_cluster     = "ny2-azd-ntnx-10"
-  bt_infra_network     = "ny2-autolab-app-ahv"
-  os_version           = "rhel7"
-  foreman_environment  = "master"
-  foreman_hostgroup    = "BT Base Server"
-  datacenter           = "ny2"
-  lob                  = "btiq"
-  additional_disks     = {
-    1 = "20"
-  }
-  external_facts       = {
-    "bt_tier" = "dev"
+locals {
+  environment    = "master"
+  hostgroup      = "BT Base Server"
+  datacenter     = "ny2"
+  image          = "rhel7"
+  network        = "ny2-autolab-app-ahv"
+  cluster        = "ny2-azd-ntnx-10"
+  facts          = {
+    "bt_product"       = "btiq"
+    "bt_tier"          = "dev"
   }
 }
 
-output "app_server_1" {
+module "btiq-vm01" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vlbtivm01"
+  alias                = "btiq-vm01"
+  bt_infra_network     = local.network
+  bt_infra_cluster     = local.cluster
+  os_version           = local.image
+  cpus                 = "2"
+  memory               = "4000"
+  foreman_environment  = local.environment
+  foreman_hostgroup    = local.hostgroup
+  external_facts       = local.facts
+  datacenter           = local.datacenter
+  additional_disks     = {
+    1 = "100"
+  }
+}
+
+output "btiq-vm01" {
   value = {
-    "fqdn"  = "${module.app_server_1.fqdn}",
-    "alias" = "${module.app_server_1.alias}",
-    "ip"    = "${module.app_server_1.ip}",
+    "fqdn"  = "${module.btiq-vm01.fqdn}",
+    "alias" = "${module.btiq-vm01.alias}",
+    "ip"    = "${module.btiq-vm01.ip}",
   }
 }
