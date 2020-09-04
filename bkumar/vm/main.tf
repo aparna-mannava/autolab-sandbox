7f400763896d7cd137fd2452aa67e531b9afc773
Brajesh.Kumar <Brajesh.Kumar@bottomline.com>
2020-09-04 21:44:56 +0530

    Updated

--- bkumar/vm/main.tf
+++ bkumar/vm/main.tf
@@ -1,45 +1,51 @@
 #
 # Building VMs for Testing
-
 terraform {
-  backend "http" {}
+  backend "s3" {}
 }
 
 locals {
-  environment    = "master"
-  hostgroup      = "BT Base Server"
-  datacenter     = "ny2"
-  image          = "rhel7"
-  network        = "ny2-autolab-app-ahv"
-  cluster        = "ny2-azd-ntnx-10"
-  facts          = {
+  lob           = "BTIQ"
+  product       = "afs"
+  image         = "rhel8"
+  environment   = "feature_BTIQ_77_jenkins"
+  hostgroup     = "BTIQ Development"
+  datacenter    = "ny2"
+  cluster       = "ny2-aza-vmw-autolab"
+  network       = "ny2-autolab-app"
+  hostname_base = "us01vl"
+  cpu           = "1"
+  memory        = "4096"
+  facts         = {
     "bt_product"       = "btiq"
     "bt_tier"          = "dev"
   }
-}
-
-module "btiq-vm01" {
-  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlbtivm01"
-  alias                = "btiq-vm01"
-  bt_infra_network     = local.network
-  bt_infra_cluster     = local.cluster
-  os_version           = local.image
-  cpus                 = "2"
-  memory               = "4000"
-  foreman_environment  = local.environment
-  foreman_hostgroup    = local.hostgroup
-  external_facts       = local.facts
-  datacenter           = local.datacenter
   additional_disks     = {
-    1 = "100"
+      1 = "100",
   }
 }
 
-output "btiq-vm01" {
+module "btiq-vmbox01" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "${local.hostname_base}btiq${local.product}01"
+  alias                = "btiq-vmbox01-${local.product}-01"
+  lob                  = local.lob
+  bt_infra_cluster     = local.cluster
+  bt_infra_network     = local.network
+  os_version           = local.image
+  foreman_environment  = local.environment
+  foreman_hostgroup    = local.hostgroup
+  datacenter           = local.datacenter
+  external_facts       = local.facts
+  cpus                 = local.cpu
+  memory               = local.memory
+  additional_disks     = local.additional_disks
+}
+
+output "btiq-vmbox01" {
   value = {
-    "fqdn"  = "${module.btiq-vm01.fqdn}",
-    "alias" = "${module.btiq-vm01.alias}",
-    "ip"    = "${module.btiq-vm01.ip}",
+    "fqdn"  = module.btiq-vmbox01.fqdn,
+    "alias" = module.btiq-vmbox01.alias,
+    "ip"    = module.btiq-vmbox01.ip,
   }
 }
