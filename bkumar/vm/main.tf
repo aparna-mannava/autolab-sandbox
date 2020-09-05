b9a37b800f4180fd09c106b83358f813919673c6
Brajesh.Kumar <Brajesh.Kumar@bottomline.com>
2020-09-05 09:44:35 +0530

    Updated

--- bkumar/vm/main.tf
+++ bkumar/vm/main.tf
@@ -3,48 +3,29 @@
 terraform {
   backend "http" {}
 }
-
-locals {
-  lob           = "BTIQ"
-  image         = "rhel7"
-  environment   = "feature_BTIQ_77_jenkins"
-  hostgroup     = "BTIQ Development"
-  datacenter    = "ny2"
-  cluster       = "ny2-aza-vmw-autolab"
-  network       = "ny2-autolab-app"
-  hostname_base = "us01vl"
-  cpu           = "1"
-  memory        = "4096"
-  facts         = {
+module "app_server_1" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vlsndbxvm01"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  foreman_environment  = "master"
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = "ny2"
+  lob                  = "btiq"
+  additional_disks     = {
+    1 = "20"
+  }
+  external_facts       = {
     "bt_product"       = "btiq"
     "bt_tier"          = "dev"
   }
-  additional_disks     = {
-      1 = "100",
-  }
 }
 
-module "btiq-vmbox01" {
-  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "${local.hostname_base}btiq${local.product}01"
-  alias                = "btiq-vmbox01-${local.product}-01"
-  lob                  = local.lob
-  bt_infra_cluster     = local.cluster
-  bt_infra_network     = local.network
-  os_version           = local.image
-  foreman_environment  = local.environment
-  foreman_hostgroup    = local.hostgroup
-  datacenter           = local.datacenter
-  external_facts       = local.facts
-  cpus                 = local.cpu
-  memory               = local.memory
-  additional_disks     = local.additional_disks
-}
-
-output "btiq-vmbox01" {
+output "app_server_1" {
   value = {
-    "fqdn"  = module.btiq-vmbox01.fqdn,
-    "alias" = module.btiq-vmbox01.alias,
-    "ip"    = module.btiq-vmbox01.ip,
+    "fqdn"  = "${module.app_server_1.fqdn}",
+    "alias" = "${module.app_server_1.alias}",
+    "ip"    = "${module.app_server_1.ip}",
   }
 }
