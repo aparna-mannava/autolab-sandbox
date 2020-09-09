e038e8daca8a551ff04fe6bfaf3ebdcd9a8e2563
Brajesh.Kumar <Brajesh.Kumar@bottomline.com>
2020-09-09 17:10:15 +0530

    modified

--- bkumar/pgins/main.tf
+++ bkumar/pgins/main.tf
@@ -6,8 +6,8 @@ terraform {
 module "pg_db01" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "us01vlsndbxpg01"
-  bt_infra_cluster     = "ny2-aze-ntnx-11"
-  bt_infra_network     = "ny2-autolab-app-ahv"
+  bt_infra_cluster     = "ny2-aze-ntnx-12"
+  bt_infra_network     = "ny2-autolab-db-ahv"
   os_version           = "rhel7"
   foreman_environment  = "master"
   foreman_hostgroup    = "BT Postgresql DB Server"
