d678ee9c65ef1995a9574e62c324dd397a493646
Saumil Patel <saumil.patel@bottomline.tech>
2020-09-22 06:43:12 +0530

    OEM and CV agent

--- saumilp/bitbkt19c/main.tf
+++ saumilp/bitbkt19c/main.tf
@@ -4,7 +4,7 @@ terraform {
 
 locals {
   product     = "cloud"
-  environment = "feature_CLOUD_70684"
+  environment = "feature_CLOUD_72066"
   datacenter  = "ny2"
   facts       = {
     "bt_tier" = "dr"
@@ -16,8 +16,7 @@ locals {
 
 module "oradb_dbserver_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlbitbkt25"
-  alias                = "cea-bbt-al2-oradb25"
+  hostname             = "us01vlbitbkt8"
   bt_infra_cluster     = "ny2-azb-ntnx-09"
   bt_infra_network     = "ny2-autolab-app-ahv"
   os_version           = "rhel7"
@@ -25,7 +24,7 @@ module "oradb_dbserver_1" {
   memory               = "4096"
   lob                  = "CLOUD"
   foreman_environment  = local.environment
-  foreman_hostgroup    = "BT Bitbucket Oracle DB Server"
+  foreman_hostgroup    = "BT Bitbucket Oracle19 Server"
   datacenter           = local.datacenter
   external_facts       = local.facts
   additional_disks     = {
@@ -39,7 +38,6 @@ module "oradb_dbserver_1" {
 output "oradb_dbserver_1" {
   value = {
     "fqdn"  = "${module.oradb_dbserver_1.fqdn}",
-    "alias" = "${module.oradb_dbserver_1.alias}",
     "ip"    = "${module.oradb_dbserver_1.ip}",
   }
 }
