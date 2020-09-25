24a87fcde0e15053d2587be6f70932f4e850660a
Michael Davidson <mdavidson@bottomline.com>
2020-09-25 09:57:38 -0400

    testing

--- mdavidson/main.tf
+++ mdavidson/main.tf
@@ -14,7 +14,11 @@ locals {
 module "rhel7-bastion" {
  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
  hostname             = "us01vlbhtst07"
- bt_infra_environment = "ny2-autolab-app"
+ bt_infra_cluster     = "ny2-aza-vmw-autolab"
+ bt_infra_network     = "ny2-autolab-app"
+ lob                  = "INF"
+ cpus                 = "2"
+ memory               = "4096"
  os_version           = "rhel7"
  foreman_environment  = local.environment
  foreman_hostgroup    = "BT Linux Bastion Hosts"
@@ -25,7 +29,11 @@ module "rhel7-bastion" {
 module "rhel8-bastion" {
  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
  hostname             = "us01vlbhtst08"
- bt_infra_environment = "ny2-autolab-app"
+ bt_infra_cluster     = "ny2-aza-vmw-autolab"
+ bt_infra_network     = "ny2-autolab-app"
+ lob                  = "INF"
+ cpus                 = "2"
+ memory               = "4096"
  os_version           = "rhel8"
  foreman_environment  = local.environment
  foreman_hostgroup    = "BT Linux Bastion Hosts"
