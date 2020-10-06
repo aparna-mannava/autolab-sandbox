a236c4e279a583956a289ac08bc64cc8fb6da717
Varadhan, Sriram <Sriram.Varadhan@bottomline.com>
2020-10-05 20:02:00 -0400

    Provison primary server

--- sriramvaradhan/dgb/main.tf
+++ sriramvaradhan/dgb/main.tf
@@ -39,3 +39,30 @@ output "oradb_server_1" {
     "ip"    = "${module.oradb_server_1.ip}",
   }
 }
+
+module "oradb_server_2" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vltdgb97"
+  bt_infra_cluster     = "ny2-aza-ntnx-07"
+  bt_infra_network     = "ny2-autolab-db-ahv"
+  os_version           = "rhel7"
+  cpus                 = "2"
+  memory               = "4096"
+  lob                  = "CLOUD"
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT DGB Oradb Server"
+  datacenter           = local.datacenter
+  external_facts       = local.facts
+  additional_disks     = {
+    1 = "200",
+    2 = "50",
+    3 = "50"
+  }
+}
+
+output "oradb_server_2" {
+  value = {
+    "fqdn"  = "${module.oradb_server_2.fqdn}",
+    "ip"    = "${module.oradb_server_2.ip}",
+  }
+}
