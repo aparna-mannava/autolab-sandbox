514efd4b72f9961d892a928c8985bed971612a3a
Varadhan, Sriram <Sriram.Varadhan@bottomline.com>
2020-10-05 22:44:47 -0400

    one more provision

--- sriramvaradhan/dgb/main.tf
+++ sriramvaradhan/dgb/main.tf
@@ -69,7 +69,7 @@ output "oradb_server_2" {
 
 module "oradb_server_3" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vltdgb99"
+  hostname             = "us01vltdgb96"
   bt_infra_cluster     = "ny2-aza-ntnx-07"
   bt_infra_network     = "ny2-autolab-db-ahv"
   os_version           = "rhel7"
