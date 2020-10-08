049fb5bdeb3ab70564926b271ccca64db679bac6
Yulzari <Shuki.Yulzari@bottomline.com>
2020-10-08 16:29:27 +0300

    change datacenter to ny2

--- cfrmiso/elastic/main.tf
+++ cfrmiso/elastic/main.tf
@@ -15,10 +15,10 @@ locals {
     #"bt_artemis_version" = "2.11.0"
     #"bt_es_version" = "7.8.0"
   }
-  datacenter = {
-    name = "colt"
-    id   = "gb03"
-  }
+  datacenter = ny2 #{
+  #   name = "colt"
+  #   id   = "gb03"
+  # }
   cfel01 = {
     hostname = "${local.hostname}el01"
     silo     = "autolablab"
@@ -29,8 +29,8 @@ module "cfel01" {
   source              = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname            = "${local.cfel01.hostname}"
   alias               = "${local.product}-${local.datacenter.id}-${local.cfel01.silo}-${local.facts.bt_role}-${local.cfel01.hostname}"
-  bt_infra_cluster    = "gb03-aza-ntnx-01"
-  bt_infra_network    = "gb03-saas-n-dev-2"
+  bt_infra_cluster    = "ny2-aza-ntnx-07"
+  bt_infra_network    = "ny2-autolab-app-ahv"
   os_version          = "rhel7"
   cpus                = "4"
   memory              = "16192"
