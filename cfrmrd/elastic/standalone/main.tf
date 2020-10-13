7ce054300b2cf86e5a2280d7a7c794ff0493599e
glevin <glevin@GLevin-800.us-bottomline.root.bottomline.com>
2020-10-13 15:42:59 +0300

    fix

--- cfrmrd/elastic/standalone/main.tf
+++ cfrmrd/elastic/standalone/main.tf
@@ -7,19 +7,32 @@ locals {
       "bt_customer" = "cfrmrd"
       "bt_product" = "cfrmrd"
       "bt_tier" = "dev"
-      "bt_env" = "qa2"
       "bt_role" = "standalone"
     }
+    es01facts    = {
+      "bt_role" = "${local.facts.bt_role}"
+      "bt_customer" = "${local.facts.bt_customer}"
+      "bt_product" = "${local.facts.bt_product}"
+      "bt_tier" = "${local.facts.bt_tier}"
+      "bt_env" = "qa"
+     }
+    es02facts    = {
+      "bt_role" = "${local.facts.bt_role}"
+      "bt_customer" = "${local.facts.bt_customer}"
+      "bt_product" = "${local.facts.bt_product}"
+      "bt_tier" = "${local.facts.bt_tier}"
+      "bt_env" = "qa2"
+     }
 }
 
-module "elasticsearch_100" {
+module "elasticsearch_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "us01vlcfrmrd500"
-  alias                = "cfrmrd-autolab-es01-standalone"
+  alias                = "cfrmrd-autolab-qa1"
   bt_infra_cluster     = "ny2-aza-ntnx-07"
   bt_infra_network     = "ny2-autolab-app-ahv"
   os_version           = "rhel7"
-  external_facts       = local.facts
+  external_facts       = local.es01facts
   lob                  = "CFRM"
   foreman_environment  = "feature_CFRMX_2451_artemis_elasticsearch_standalone"
   foreman_hostgroup    = "CFRMRD ElasticSearch And Artemis Standalone2"
@@ -32,10 +45,38 @@ module "elasticsearch_100" {
   }
 } 
 
-output "elasticsearch_100" {
-  value = {
-    "fqdn"  = "${module.elasticsearch_100.fqdn}",
-    "alias" = "${module.elasticsearch_100.alias}",
-    "ip"    = "${module.elasticsearch_100.ip}",
+module "elasticsearch_2" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vlcfrmrd501"
+  alias                = "cfrmrd-autolab-qa2"
+  bt_infra_cluster     = "ny2-aza-ntnx-07"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  external_facts       = local.es02facts
+  lob                  = "CFRM"
+  foreman_environment  = "feature_CFRMX_2451_artemis_elasticsearch_standalone"
+  foreman_hostgroup    = "CFRMRD ElasticSearch And Artemis Standalone2"
+  datacenter           = "ny2"
+  cpus                 = "2"
+  memory         	   = "4096"
+  additional_disks     = {
+    1 = "50",
+    2 = "100"
   }
-}  
+}
+
+output "elasticsearch_1" {
+  value = {
+    "fqdn"  = "${module.elasticsearch_1.fqdn}",
+    "alias" = "${module.elasticsearch_1.alias}",
+    "ip"    = "${module.elasticsearch_1.ip}",
+  }
+}
+
+output "elasticsearch_2" {
+  value = {
+    "fqdn"  = "${module.elasticsearch_2.fqdn}",
+    "alias" = "${module.elasticsearch_2.alias}",
+    "ip"    = "${module.elasticsearch_2.ip}",
+  }
+}
