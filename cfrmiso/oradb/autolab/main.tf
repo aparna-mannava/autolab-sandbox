879a32a3b653336eb4c7b261287216a3a9594904
Yulzari <Shuki.Yulzari@bottomline.com>
2020-10-14 17:58:10 +0300

    adding under score

--- cfrmiso/oradb/autolab/main.tf
+++ cfrmiso/oradb/autolab/main.tf
@@ -4,7 +4,7 @@ terraform {
 
 locals {
   product     = "cfrmiso"
-  environment = "CFRMSUP_1565_automate_oracledb" # Change to nonprod after 2020-02-11 Puppet release
+  environment = "CFRMSUP_1565__automate_oracledb" # Change to nonprod after 2020-02-11 Puppet release
   hostname    = "us01vlcfdb"
   hostgroup   = "CFRM BT ISO IL Oracle DB Servers"
   facts = {
