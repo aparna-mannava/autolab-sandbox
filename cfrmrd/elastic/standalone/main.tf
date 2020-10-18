47c59f58220ba94074e76e47a728bc85f6a72be1
glevin <glevin@GLevin-800.us-bottomline.root.bottomline.com>
2020-10-18 09:49:52 +0300

    fix

--- cfrmrd/elastic/standalone/main.tf
+++ cfrmrd/elastic/standalone/main.tf
@@ -36,7 +36,7 @@ module "elasticsearch_1" {
   external_facts       = local.es01facts
   lob                  = "CFRM"
   foreman_environment  = "feature_CFRMX_2451_artemis_elasticsearch_standalone"
-  foreman_hostgroup    = "CFRMRD ElasticSearch And Artemis Standalone2"
+  foreman_hostgroup    = "CFRMRD ElasticSearch And Artemis Standalone"
   datacenter           = "ny2"
   cpus                 = "2"
   memory         	   = "4096"
@@ -56,7 +56,7 @@ module "elasticsearch_2" {
   external_facts       = local.es02facts
   lob                  = "CFRM"
   foreman_environment  = "feature_CFRMX_2451_artemis_elasticsearch_standalone"
-  foreman_hostgroup    = "CFRMRD ElasticSearch And Artemis Standalone2"
+  foreman_hostgroup    = "CFRMRD ElasticSearch And Artemis Standalone"
   datacenter           = "ny2"
   cpus                 = "2"
   memory         	   = "4096"
