95698a8e36c6e7ac5f076ec3f33889c9a9b20f06
glevin <glevin@GLevin-800.us-bottomline.root.bottomline.com>
2020-10-20 12:13:27 +0300

    fix

--- cfrmrd/hapostgres/hapg/main.tf
+++ cfrmrd/hapostgres/hapg/main.tf
@@ -11,8 +11,6 @@ locals {
   hapg_aliases    = ["cfrmrd-hapg1","cfrmrd-hapg2","cfrmrd-hapg3"]
   haproxy_alias   = ["cfrmrd-proxy"]
   domain          = "auto.saas-n.com"
-  tier            = "autolab"
-  bt_env          = "1"
   bt_product      = "cfrmrd"
   lob             = "CFRM"
   hostgroup       = "CFRMRD HA PG Server"
@@ -20,10 +18,8 @@ locals {
   cluster         = "ny2-aza-ntnx-07"
   network         = "ny2-autolab-app-ahv"
   facts_hapg      = {
-    "bt_env"                  = "${local.bt_env}"
-    "bt_tier"                 = "${local.tier}"
     "bt_product"              = "${local.bt_product}"
-     bt_role                  = "postgresql"
+    "bt_role"                 = "postgresql"
     "bt_etcd_cluster_members" = ["${local.etcd_servers[0]}.${local.domain}", "${local.etcd_servers[1]}.${local.domain}", "${local.etcd_servers[2]}.${local.domain}"]
     "bt_hapg_cluster_members" = ["${local.hapg_servers[0]}.${local.domain}", "${local.hapg_servers[1]}.${local.domain}"]
     "bt_hapg_node1"           = "${local.hapg_servers[0]}.${local.domain}"
@@ -36,7 +32,7 @@ locals {
     "bt_env"                  = "${local.bt_env}"
     "bt_tier"                 = "${local.tier}"
     "bt_product"              = "${local.bt_product}"
-     bt_role                  = "haproxy"
+    "bt_role"                 = "haproxy"
     "bt_etcd_cluster_members" = ["${local.etcd_servers[0]}.${local.domain}", "${local.etcd_servers[1]}.${local.domain}", "${local.etcd_servers[2]}.${local.domain}"]
     "bt_hapg_cluster_members" = ["${local.hapg_servers[0]}.${local.domain}", "${local.hapg_servers[1]}.${local.domain}"]
     "bt_hapg_node1"           = "${local.hapg_servers[0]}.${local.domain}"
@@ -47,7 +43,7 @@ locals {
   }
 }
 
-module "ny2_autolab_hapg_0" {
+module "cfrmrd_autolab_hapg_0" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "${local.hapg_servers[0]}"
   alias                = "${local.hapg_aliases[0]}"
@@ -67,7 +63,7 @@ module "ny2_autolab_hapg_0" {
   }
 }
 
-module "ny2_autolab_hapg_1" {
+module "cfrmrd_autolab_hapg_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "${local.hapg_servers[1]}"
   alias                = "${local.hapg_aliases[1]}"
@@ -87,7 +83,7 @@ module "ny2_autolab_hapg_1" {
   }
 }
 
-module "ny2_autolab_hapg_2" {
+module "cfrmrd_autolab_hapg_2" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "${local.hapg_servers[2]}"
   alias                = "${local.hapg_aliases[2]}"
@@ -107,7 +103,7 @@ module "ny2_autolab_hapg_2" {
   }
 }
 
-module "ny2_autolab_haproxy_1" {
+module "cfrmrd_autolab_haproxy_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "${local.haproxy_server[0]}"
   alias                = "${local.haproxy_alias[0]}"
@@ -127,34 +123,34 @@ module "ny2_autolab_haproxy_1" {
   }     
 }
 
-output "ny2_autolab_hapg_0" {
+output "cfrmrd_autolab_hapg_0" {
   value = {
-    "fqdn"  = "${module.ny2_autolab_hapg_0.fqdn}",
-    "alias" = "${module.ny2_autolab_hapg_0.alias}",
-    "ip"    = "${module.ny2_autolab_hapg_0.ip}",
+    "fqdn"  = "${module.cfrmrd_autolab_hapg_0.fqdn}",
+    "alias" = "${module.cfrmrd_autolab_hapg_0.alias}",
+    "ip"    = "${module.cfrmrd_autolab_hapg_0.ip}",
   }
 }
 
-output "ny2_autolab_hapg_1" {
+output "cfrmrd_autolab_hapg_1" {
   value = {
-    "fqdn"  = "${module.ny2_autolab_hapg_1.fqdn}",
-    "alias" = "${module.ny2_autolab_hapg_1.alias}",
-    "ip"    = "${module.ny2_autolab_hapg_1.ip}",
+    "fqdn"  = "${module.cfrmrd_autolab_hapg_1.fqdn}",
+    "alias" = "${module.cfrmrd_autolab_hapg_1.alias}",
+    "ip"    = "${module.cfrmrd_autolab_hapg_1.ip}",
   }
 }
 
-output "ny2_autolab_hapg_2" {
+output "cfrmrd_autolab_hapg_2" {
   value = {
-    "fqdn"  = "${module.ny2_autolab_hapg_2.fqdn}",
-    "alias" = "${module.ny2_autolab_hapg_2.alias}",
-    "ip"    = "${module.ny2_autolab_hapg_2.ip}",
+    "fqdn"  = "${module.cfrmrd_autolab_hapg_2.fqdn}",
+    "alias" = "${module.cfrmrd_autolab_hapg_2.alias}",
+    "ip"    = "${module.cfrmrd_autolab_hapg_2.ip}",
   }
 }
 
-output "ny2_autolab_haproxy_1" {
+output "cfrmrd_autolab_haproxy_1" {
   value = {
-    "fqdn"  = "${module.ny2_autolab_haproxy_1.fqdn}",
-    "alias" = "${module.ny2_autolab_haproxy_1.alias}",
-    "ip"    = "${module.ny2_autolab_haproxy_1.ip}",
+    "fqdn"  = "${module.cfrmrd_autolab_haproxy_1.fqdn}",
+    "alias" = "${module.cfrmrd_autolab_haproxy_1.alias}",
+    "ip"    = "${module.cfrmrd_autolab_haproxy_1.ip}",
   }
 }    
