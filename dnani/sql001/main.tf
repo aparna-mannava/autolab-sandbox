8ebe0f52a2d6ca0e990c450c4c00adba05a22730
Dagani <Nani.Dagani@bottomline.com>
2020-11-04 13:54:49 -0500

    modified cluster node

--- dnani/sql001/main.tf
+++ dnani/sql001/main.tf
@@ -17,7 +17,7 @@ module "inf_windns_nd1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "${local.hostname}"
   alias                = ""
-  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_cluster     = "ny2-aza-ntnx-13"
   bt_infra_network     = "ny2-autolab-app-ahv"
   lob                  = "inf"
   os_version           = "win2016"
