92161f3d1ffaf040d4c8a892875184738ed3127f
Stephen da Conceicao <stephen.daconceicao@bottomline.com>
2020-11-05 10:39:22 -0500

    GLU-3333 UX Nexus

---
+++ stephen.daconceicao/nexus/main.tf
@@ -0,0 +1,50 @@
+terraform {
+ backend "s3" {}
+}
+
+locals {
+    facts = {
+        "bt_tier" = "dev"
+        "bt_env" = "1",
+        "bt_product" = "ux"
+        "bt_role" = "nexus"
+    }
+    cluster = "ny2-aza-ntnx-13"
+    network = "ny2-autolab-app-ahv"
+    os      = "rhel8"
+    cpus    = "8"
+    memory  = "8192"
+    additional_disks = {
+        1 = "500",
+        2 = "500"
+    }
+    environment = "feature_GLU_3333_glu_nexus"
+    hostgroup   = "UX Nexus"
+    datacenter  = "ny2"
+    lob         = "CLOUD"
+}
+
+module "jenkins" {
+    source              = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+    hostname            = "us01vluxnex001"
+    alias               = "nexus-ux"
+    bt_infra_cluster    = local.cluster
+    bt_infra_network    = local.network
+    os_version          = local.os
+    cpus                = local.cpus
+    memory              = local.memory
+    external_facts      = local.facts
+    foreman_environment = local.environment
+    additional_disks    = local.additional_disks
+    foreman_hostgroup   = local.hostgroup
+    datacenter          = local.datacenter
+    lob                 = local.lob
+}
+
+output "jenkins" {
+ value = {
+ "fqdn" = "${module.jenkins.fqdn}",
+ "alias" = "${module.jenkins.alias}",
+ "ip" = "${module.jenkins.ip}",
+ }
+}
