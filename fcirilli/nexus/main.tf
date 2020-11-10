e5466e1b37515b6db5644d5067ecd6ba2c08944f
Fabio <fcirilli@bottomline.com>
2020-11-10 18:05:52 +0100

    fix

--- fcirilli/nexus/main.tf
+++ fcirilli/nexus/main.tf
@@ -8,7 +8,7 @@ terraform {
 locals {
   environment    = "feature_FMDO_1928_saas_n_puppet_profile_for_nexus"
   datacenter     = "ny2"
-  datastore      = "ny2-dgb-development"
+  network      = "ny2-autolab-app-ahv"
   cluster        = "ny2-aza-ntnx-13"
   facts          = {
     "bt_tier"          = "dev"
@@ -20,13 +20,14 @@ module "nexus" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "us01vlfmnxs01"
   alias                = "fmcloud-nexus-e2e"
-  bt_infra_network     = local.datastore
+  bt_infra_network     = local.network
   bt_infra_cluster     = local.cluster
+  lob                  = "FMCLOUD"
   os_version           = "rhel7"
   cpus                 = "4"
   memory               = "8192"
   foreman_environment  = local.environment
-  foreman_hostgroup    = "FM Cloud Nexus3 Server"
+  foreman_hostgroup    = "BT FM Cloud Nexus3 Server"
   external_facts       = local.facts
   datacenter           = local.datacenter
   additional_disks     = {
