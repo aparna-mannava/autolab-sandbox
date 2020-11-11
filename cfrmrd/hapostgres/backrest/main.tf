f99d027f1555a661e6408fcf36c0b3ccd9a68f54
glevin <glevin@GLevin-800.us-bottomline.root.bottomline.com>
2020-11-11 08:21:21 +0200

    fix

--- cfrmrd/hapostgres/backrest/main.tf
+++ cfrmrd/hapostgres/backrest/main.tf
@@ -25,6 +25,7 @@ locals {
     "bt_hapg_node3"           = "${local.hapg_servers[2]}.${local.domain}"
     "bt_backup_node"          = "${local.backrest_server[0]}.${local.domain}"
     "bt_cluster_name"         = "cfrmrd_cluster"
+    "bt_pg_version"           = "12"
   }
 }
 
