60e7ec1deae7fdb6cbec4766682c0d5700de5187
LaBranche <Laura.LaBranche@bottomline.com>
2020-11-13 10:30:04 -0500

    ora2pg test build

---
+++ llabranche/ora2pg/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  backend "s3" {}
+}
+locals {
+  facts       = {
+    "bt_tier"    = "auto"
+    "bt_product" = "shared"
+    "bt_role" = "postgresql"
+    "bt_env"    = "2"
+    "bt_pg_version" = "12"
+  }
+}
+
+module "pgora_db_1" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vltpgbora02" 
+  alias                = "pgoratest02" 
+  bt_infra_cluster     = "ny2-aza-ntnx-13"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  foreman_environment  = "master"
+  foreman_hostgroup    = "BT Postgresql DB Server" 
+  datacenter           = "ny2"
+  lob                  = "cloud"
+  cpus                 = "4"
+  memory               = "4098"
+  additional_disks     = {
+    1 = "100"
+    2 = "50"
+  }
+  external_facts       = local.facts
+}
+
+output "pgora_db_1" {
+  value = {
+    "fqdn"  = "${module.pgora_db_1.fqdn}",
+    "alias" = "${module.pgora_db_1.alias}",
+    "ip"    = "${module.pgora_db_1.ip}",
+  }
+  
+}
