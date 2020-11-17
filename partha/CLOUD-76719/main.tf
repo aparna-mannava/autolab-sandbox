96111638f209dc58d41ccf1b73a6f86f0ff7837c
Hassan Parthasarathy <hparthasarathy@bottomline.com>
2020-11-17 13:14:34 -0500

    Added file for 2019 Server

--- partha/CLOUD-76719/main.tf
+++ partha/CLOUD-76719/main.tf
@@ -29,7 +29,7 @@ module "inf_windns_hsp" {
   datacenter           = "${local.datacenter}"
   additional_disks     = {
     1 = "200",
-    2 = "94",
+    2 = "95",
     3 = "50",
     4 = "50",
     5 = "50",
