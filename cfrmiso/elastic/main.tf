a65132afdc6c93faef022a1091fb8e11d53c81bd
Yulzari <Shuki.Yulzari@bottomline.com>
2020-11-30 09:33:13 +0200

    recreate elk

--- cfrmiso/elastic/main.tf
+++ cfrmiso/elastic/main.tf
@@ -41,7 +41,7 @@ module "cfel01" {
   foreman_hostgroup   = "${local.hostgroup}"
   datacenter          = "${local.datacenter.name}"
   additional_disks     = {
-    1 = "100",  //  disk 1
+    1 = "100",  //   disk 1
   }
 }
 
