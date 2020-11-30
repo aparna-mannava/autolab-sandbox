dc66749d4809b0e1920683bfd02786713cac17b4
Lucas Emery <lemery@bottomline.com>
2020-11-30 04:57:57 -0500

    Adding some workers

--- lemery/k8s/main.tf
+++ lemery/k8s/main.tf
@@ -5,8 +5,11 @@ terraform {
 module "k8s_inf_us01auto1" {
   source              = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-k8s-infrastructure.git?ref=master"
   k8s_masters         = ["us01vlkm001", "us01vlkm002", "us01vlkm003"]
+  k8s_workers         = ["us01vlkw001", "us01vlkw002"]
   master_cpus         = 8
+  worker_cpus         = 4
   master_memory       = 16384
+  worker_memory       = 4096
   domain              = "auto.saas-n.com"
   bt_infra_cluster    = "ny2-aza-ntnx-05"
   bt_infra_network    = "ny2-autolab-app-ahv"
