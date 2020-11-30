25edbb20b0fa166fb5603babb9529da16a5e85f8
Lucas Emery <lemery@bottomline.com>
2020-11-30 03:35:54 -0500

    update main

--- lemery/k8s/main.tf
+++ lemery/k8s/main.tf
@@ -2,32 +2,21 @@ terraform {
   backend "s3" {}
 }
 
-resource "random_string" "token1" {
-  length  = 6
-  upper   = false
-  special = false
-}
-
-resource "random_string" "token2" {
-  length  = 16
-  upper   = false
-  special = false
-}
-
-module "kubernetes" {
-  source                  = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-kubernetes.git?ref=master"
-  cluster_token           = "${random_string.token1.result}.${random_string.token2.result}"
-  additional_disks        = ["100"]
-  datacenter              = "ny2"
-  domain                  = "auto.saas-n.com"
-  foreman_environment     = "feature_CEA_6213_k8s_svc_ip_ranges"
-  k8s_masters             = ["us01vlkm01","us01vlkm02","us01vlkm03"]
-  k8s_workers             = ["us01vlkw01","us01vlkw02"]
-  master_cpus             = 4
-  master_memory           = 8192
-  os_version              = "rhel7"
-  worker_cpus             = 4
-  worker_memory           = 8192
-  bt_infra_cluster        = "ny2-aza-ntnx-05"
-  bt_infra_network        = "ny2-autolab-app-ahv"
+module "k8s_inf_us01auto1" {
+  source              = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-k8s-infrastructure.git?ref=master"
+  k8s_masters         = ["us01vlkm001", "us01vlkm002", "us01vlkm003"]
+  master_cpus         = 8
+  master_memory       = 16384
+  domain              = "auto.saas-n.com"
+  bt_infra_cluster    = "ny2-aza-ntnx-05"
+  bt_infra_network    = "ny2-autolab-app-ahv"
+  datacenter          = "ny2"
+  os_version          = "rhel7"
+  foreman_environment = "master"
+  additional_disks    = ["100"]
+  external_facts = {
+    "bt_product" = "inf"
+    "bt_tier"    = "autolab"
+    "bt_env"     = "94"
+  }
 }
