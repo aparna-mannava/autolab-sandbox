da31149fec534932c923ca0da3584cf040dcace8
Chris Drew <cdrew@bottomline.com>
2020-12-03 15:03:41 -0500

    finding space

--- cdrew/jdbcss/main.tf
+++ cdrew/jdbcss/main.tf
@@ -14,7 +14,7 @@ module "pg-service" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "us01vlsstst1" 
   alias                = "sstest01" 
-  bt_infra_cluster     = "ny2-aze-ntnx-12"
+  bt_infra_cluster     = "ny5-aza-ntnx-14"
   bt_infra_network     = "ny2-autolab-app-ahv"
   os_version           = "rhel7"
   foreman_environment  = "master"
