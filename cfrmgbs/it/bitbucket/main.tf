48caacf0283256abc4029f4a9665fb4e0f507f6b
shuki.yulzari <shuki.yulzari@bottomline.com>
2021-01-14 16:09:56 +0200

    bitbucket

---
+++ cfrmgbs/it/bitbucket/main.tf
@@ -0,0 +1,114 @@
+terraform {
+  backend "http" {}
+}
+#  Build Bitbucket and HAProxy #
+locals {
+  product     = "cfrmit"
+  environment = "master"   
+  hostname    = "il02"
+  facts = {
+    "bt_product"     = "cfrmit"
+    "bt_customer"    = "it"
+    "bt_tier"        = "prod"
+    "bt_role"        = "bitbucket"
+    "bt_host_number" = "001"
+  }
+  datacenter = {
+    name = "ny2"
+    id   = "il02"
+  }
+  #|#################################################|#  
+  #|## -- create Bitbucket and HAProxy servers -- ###|#
+  #|#################################################|#
+
+  #|## Bitbucket server module configuration ########|#
+  cfbb001 = { 
+    hostname    = "${local.hostname}vlbb${local.facts.bt_host_number}"
+    alias       = "${local.hostname}vlbitbucket${local.facts.bt_host_number}"
+    silo        = "prod"
+    hostgroup   = "BT CFRM IT Management" 
+    facts       = {
+      "bt_product"  = "${local.facts.bt_product}"
+      "bt_customer" = "${local.facts.bt_customer}"
+      "bt_tier"     = "${local.facts.bt_tier}"
+      "bt_role"     = "${local.facts.bt_role}"
+      "bt_app"      = "bitbucket"
+      "bt_fw_group" = "CFRMRD_NY02_BH_TO_CFRMRD_PR_BB" }
+  }
+  
+  #|## HAProxy server module configuration ########|#
+  cfpx001 = { 
+    hostname    = "${local.hostname}vlprx${local.facts.bt_host_number}"
+    alias       = "${local.hostname}vlhaproxy${local.facts.bt_host_number}"
+    silo        = "prod"
+    hostgroup   = "BT CFRM IT Management"
+    facts       = {
+      "bt_product"  = "${local.facts.bt_product}"
+      "bt_customer" = "${local.facts.bt_customer}"
+      "bt_tier"     = "${local.facts.bt_tier}"
+      "bt_role"     = "${local.facts.bt_role}"
+      "bt_app"      = "haproxy"
+      "bt_fw_group" = "CFRMRD_NY02_BH_TO_CFRMRD_PR_HA" }
+  }
+}
+module "cfbb001" {
+  source              = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname            = "${local.cfbb001.hostname}"
+  alias               = "${local.cfbb001.alias}"
+  ## saas-p NY2 on IL02 subnet
+  bt_infra_cluster    = "il02-aza-ntnx-01"
+  bt_infra_network    = "il02_hosted_corp_app"
+  os_version          = "rhel7"
+  cpus                = "4"
+  memory              = "32768"
+  lob                 = "CFRM"
+  external_facts      = "${local.cfbb001.facts}"
+  foreman_environment = "${local.environment}"
+  foreman_hostgroup   = "${local.cfbb001.hostgroup}"
+  datacenter          = "${local.datacenter.name}"
+  additional_disks    = {
+    1 = "250", // disk1
+  }
+}
+
+module "cfpx001" {
+  source              = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname            = "${local.cfpx001.hostname}" 
+  alias               = "${local.cfpx001.alias}" 
+  ## saas-p NY2 on IL02 subnet
+  #bt_infra_cluster    = "il02-aza-ntnx-01"
+  #bt_infra_network    = "il02_hosted_corp_app"
+  ## auto.saas-n
+  bt_infra_cluster    = "ny5-aza-ntnx-14"
+  bt_infra_network    = "ny2-autolab-app-ahv"
+  os_version          = "rhel7"
+  cpus                = "2"
+  memory              = "2048"
+  lob                 = "CFRM"
+  external_facts      = "${local.cfpx001.facts}"
+  foreman_environment = "${local.environment}"
+  foreman_hostgroup   = "${local.cfpx001.hostgroup}"
+  datacenter          = "${local.datacenter.name}"
+    additional_disks  = {
+    1 = "50", // disk1
+  }
+}
+
+
+output "cfbb001" {
+  value = {
+    "fqdn"  = "${module.cfbb001.fqdn}",
+    "alias" = "${module.cfbb001.alias}",
+    "ip"    = "${module.cfbb001.ip}",
+    "app"   = "${local.cfbb001.facts.bt_app}"
+  }
+}
+
+output "cfpx001" {
+  value = {
+    "fqdn"  = "${module.cfpx001.fqdn}",
+    "alias" = "${module.cfpx001.alias}",
+    "ip"    = "${module.cfpx001.ip}",
+    "app"   = "${local.cfpx001.facts.bt_app}"
+  }
+}
