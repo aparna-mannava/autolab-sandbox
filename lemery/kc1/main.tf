f12dcaf3c2e73cd4b06ac7ca4067ef5f98a83869
Lucas Emery <lemery@bottomline.com>
2021-01-22 16:23:39 -0500

    Adding platform servers

--- lemery/kc1/main.tf
+++ lemery/kc1/main.tf
@@ -3,19 +3,20 @@ terraform {
 }
 
 locals {
-  lob           = "cea"
-  image         = "rhel7"
-  hostgroup     = "BT Small Kafka Broker Server"
-  environment   = "master"
-  datacenter    = "ny2"
-  cluster       = "ny2-azd-ntnx-10"
-  network       = "ny2-autolab-app-ahv"
-  cpus          = "4"
-  memory        = "8192"
-  disks     = {
+  lob                = "cea"
+  image              = "rhel7"
+  hostgroup          = "BT Small Kafka Broker Server"
+  platform_hostgroup = "BT Small Kafka Platform Server"
+  environment        = "master"
+  datacenter         = "ny2"
+  cluster            = "ny2-azd-ntnx-10"
+  network            = "ny2-autolab-app-ahv"
+  cpus               = "4"
+  memory             = "8192"
+  disks              = {
     1 = "700",
   }
-  facts         = {
+  facts              = {
     "bt_tier" = "ny2"
     "bt_env" = "99"
     "bt_confluent_cluster" = {
@@ -73,6 +74,36 @@ module "base_server_3" {
   memory               = local.memory
   foreman_environment  = local.environment
   foreman_hostgroup    = local.hostgroup
+  datacenter           = local.datacenter
+  external_facts       = local.facts
+  lob                  = local.lob
+}
+
+module "base_server_4" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vlskfkp100"
+  bt_infra_cluster     = local.cluster
+  bt_infra_network     = local.network
+  os_version           = local.image
+  cpus                 = local.cpus
+  memory               = local.memory
+  foreman_environment  = local.environment
+  foreman_hostgroup    = local.platform_hostgroup
+  datacenter           = local.datacenter
+  external_facts       = local.facts
+  lob                  = local.lob
+}
+
+module "base_server_5" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vlskfkp101"
+  bt_infra_cluster     = local.cluster
+  bt_infra_network     = local.network
+  os_version           = local.image
+  cpus                 = local.cpus
+  memory               = local.memory
+  foreman_environment  = local.environment
+  foreman_hostgroup    = local.platform_hostgroup
   datacenter           = local.datacenter
   external_facts       = local.facts
   lob                  = local.lob
