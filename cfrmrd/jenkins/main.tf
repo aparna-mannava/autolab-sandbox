11012af2a3a6ae2d1683f209ca50018fbc515c94
glevin <glevin@bottomline.com>
2021-01-24 15:31:41 +0200

    fx

--- cfrmrd/jenkins/main.tf
+++ cfrmrd/jenkins/main.tf
@@ -17,7 +17,7 @@ locals {
       "bt_env" = "${local.facts.bt_env}"
      }
 }
-   
+    
 module "staging_jenkins" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "us01vlcfrmrd223"
