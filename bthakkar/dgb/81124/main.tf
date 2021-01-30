471497b516292793ecf387101f31981229a284c1
Bhavesh Thakkar <bhavesh.thakkar@bottomline.com>
2021-01-30 06:12:47 -0500

    ttest

---
+++ bthakkar/dgb/81124/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  backend "s3" {}
+}
+
+locals {
+  lob           = "dgb"
+  puppet_env    = "master"
+  datacenter    = "ny2"
+  domain        = "auto.saas-n.com"
+  facts         = {
+    "bt_customer"         = "fi0007" #ex: fiXXXX
+    "bt_tier"             = "pr" #ex: sbx, tst, td, demo
+    "bt_env"              = "" #ex: leave blank for first env, or non-zero-padded number
+    "bt_product"          = "dgb"
+    "bt_product_version"  = "3.6"
+  }
+}
+
+module "db_server_1" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us00vlauto057" #ex: us01vldgbdbXXX
+  os_version           = "rhel7"
+  alias                = "${local.lob}-pci-${local.facts.bt_tier}${local.facts.bt_env}-${local.facts.bt_customer}-oradb01"
+  datacenter           = local.datacenter
+  bt_infra_network     = "ny2-autolab-db-ahv"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  foreman_environment  = local.puppet_env
+  foreman_hostgroup    = "BT DGB Oradb Server" #ex: BT DGB Oradb Server
+  external_facts       = local.facts
+  cpus                 = "8"
+  memory               = "65536"
+  additional_disks     = {
+    1 = "200",
+  }
+}
+
+
+output "db_server_1" {
+  value = {
+    "fqdn"  = "${module.db_server_1.fqdn}",
+    "alias" = "${module.db_server_1.alias}",
+    "ip"    = "${module.db_server_1.ip}",
+  }
+}
