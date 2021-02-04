91dfa76e4c42123e3938db20196182a62caad8a7
fmasihuddin <fmasihuddin@bottomline.com>
2021-02-04 16:03:23 -0500

    Trying out creating a VM with Windows 2016 server and SQL Server 2019

---
+++ fmasihuddin/createform/tfs/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  backend "http" {}
+}
+
+locals {
+  product        = "cfm"
+  environment    = "master"
+  datacenter     = "ny2"
+  hostname       = "us01tfssql2016sp1"
+  hostgroup      = "BT BFS MSSQL 2019 Server"
+  lob			 = "fda"
+  facts          = {
+    "bt_product"      = "cfm"
+    "bt_tier"         = "dev"
+    "bt_bfs_timezone" = "Eastern Standard Time"
+  }
+}
+
+
+module "cfm_tfs_server" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "${local.hostname}"
+  alias                = "${local.lob}-${local.facts.bt_product}-${local.facts.bt_tier}-${local.hostname}"
+  bt_infra_cluster     = "ny2-aza-ntnx-13"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  lob                  = "${local.lob}"
+  os_version           = "win2016"
+  cpus                 = "2"
+  memory               = "4096"
+  external_facts       = "${local.facts}"
+  foreman_environment  = "${local.environment}"
+  foreman_hostgroup    = "${local.hostgroup}"
+  datacenter           = "${local.datacenter}"
+  additional_disks     = {
+    1 = "200",
+    2 = "100",
+    3 = "100",
+    4 = "100",
+    5 = "50",
+    6 = "50"
+  }
+}
+
+output "inf_windns_SRVR" {
+  value = {
+    "fqdn"  = "${module.inf_windns_SRVR.fqdn}",
+    "alias" = "${module.inf_windns_SRVR.alias}",
+    "ip"    = "${module.inf_windns_SRVR.ip}",
+  }
+}
