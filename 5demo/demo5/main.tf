10bd11683160037696717c48a489b391aaa17385
Eitan Rimon <eperkal@bottomline.com>
2021-02-07 10:34:05 +0200

    d5

--- 5demo/demo5/main.tf
+++ 5demo/demo5/main.tf
@@ -3,58 +3,59 @@ terraform {
 }
 #  Build test server
 locals {
- # product     = "cfrmit"
+  product     = "cfrmit"
   environment = "master" 
   hostname    = "us01"
   facts = {
-    "bt_product"     = "cfrmit"
-    "bt_role"     = "demo"
-    "bt_host_number" = "0005"
-    "bt_tier"     = "autolab"
+    "bt_product"     = "cfrmiso"
+    "bt_role"     = "mgmt"
+    "bt_host_number" = "001"
   }
   datacenter = {
     name = "ny2"
-    id   = "ny2"
+    id   = "ny2" 
   }
 
   #|## Demo server module configuration ########|#
-  app005 = { 
-    hostname    = "${local.hostname}VLDEMO${local.facts.bt_host_number}"
-    alias       = "${local.hostname}VLDEMO${local.facts.bt_host_number}"
+  demo5 = { 
+    hostname    = "${local.hostname}vldemo5${local.facts.bt_host_number}"
+    alias       = "${local.hostname}vldemo5${local.facts.bt_host_number}"
     silo        = "autolab"
-    hostgroup   = "BT CFRM Demo Servers"
+    hostgroup   = "BT CFRM Demo Servers" 
+    facts       = {
+      "bt_product"  = "cfrmiso"
+      "bt_role"     = "mgmt"
+      "bt_tier"     = "autolab"
+  }
 }
 }
-module "app005" {
+module "demo5" {
   source              = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname            = "${local.app005.hostname}"
-  alias               = "${local.app005.alias}"
+  hostname            = "${local.demo5.hostname}"
+  alias               = "${local.demo5.alias}"
   ## saas-p NY2 on IL02 subnet
-  # bt_infra_cluster    = "il02-aza-ntnx-01"
-  # bt_infra_network    = "il02_hosted_corp_app"
+  #bt_infra_cluster    = "il02-aza-ntnx-01"
+  #bt_infra_network    = "il02_hosted_corp_app"
   ## auto.saas-n
-  # bt_infra_cluster    = "il02-aza-ntnx-01"
-  # bt_infra_network    = "il02-hosted-cfrm-dmz"
-
   bt_infra_cluster    = "ny5-aza-ntnx-14"
   bt_infra_network    = "ny2-autolab-app-ahv"
   os_version          = "rhel7"
   cpus                = "4"
   memory              = "8096"
   lob                 = "CFRM"
-  external_facts      = "${local.app005.facts}"
+  external_facts      = "${local.demo5.facts}"
   foreman_environment = "${local.environment}"
-  foreman_hostgroup   = "${local.app005.hostgroup}"
+  foreman_hostgroup   = "${local.demo5.hostgroup}"
   datacenter          = "${local.datacenter.name}"
   additional_disks    = {
-    1 = "160", // disk1 100gb 
+    1 = "100", // disk1 100gb 
   }
 }
 
-output "app005" {
+output "demo5" {
   value = {
-    "fqdn"  = "${module.app005.fqdn}",
-    "alias" = "${module.app005.alias}",
-    "ip"    = "${module.app005.ip}"
+    "fqdn"  = "${module.demo5.fqdn}",
+    "alias" = "${module.demo5.alias}",
+    "ip"    = "${module.demo5.ip}"
   }
 }
