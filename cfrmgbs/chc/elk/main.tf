336c84e3efefc86ee6051a51062d71613bdeb526
Eitan Rimon <eperkal@bottomline.com>
2021-03-08 14:05:20 +0200

    d

--- cfrmgbs/chc/elk/main.tf
+++ cfrmgbs/chc/elk/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  backend "s3" {}
+  backend "http" {}
 }
 
 locals {
@@ -102,5 +102,4 @@ output "chcelk03" {
     "fqdn"  = "${module.chcelk03.fqdn}",
     "alias" = "${module.chcelk03.alias}",
     "ip"    = "${module.chcelk03.ip}",
-  }
 }
