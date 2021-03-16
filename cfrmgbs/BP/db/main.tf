052904b76f2f5ae92991ca12c6d68aaccc571dcd
Shuki.Yulzari <shuki.yulzari@bottomline.com>
2021-03-16 17:11:03 +0200

    rebuild oracle DB

--- cfrmgbs/BP/db/main.tf
+++ cfrmgbs/BP/db/main.tf
@@ -38,7 +38,7 @@ module "dblab_1" {
   alias                = "${local.facts.bt_product}.${local.facts.bt_customer}.${local.facts.bt_tier}.${local.datacenter.id}.db01"// cfrmcloud.bp.prod.gb00.db01
   bt_infra_cluster     = "${local.facts.bt_infra_cluster}"
   bt_infra_network     = "${local.facts.bt_infra_network}"
-  #firewall_group       = "${local.facts.firewall_group}" // adding firewall group
+  #firewall_group       = "${local.facts.firewall_group}" //  adding firewall group
   lob                  = "${local.facts.bt_lob}"
   foreman_environment  = "${local.facts.environment}"
   foreman_hostgroup    = "${local.facts.hostgroup}"
