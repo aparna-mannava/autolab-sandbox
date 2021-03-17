07fe42bbdc57a6f06043147e971bf45ecb3713e5
Vyas <Krunal.Vyas@bottomline.com>
2021-03-17 04:44:21 -0400

    DGB_PCI

--- krunal/dgb_pr/main.tf
+++ krunal/dgb_pr/main.tf
@@ -17,7 +17,7 @@ locals {
 module "oradb_dbserver_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "us01vldbcnbpr"
-  bt_infra_cluster     = "ny2-azb-ntnx-09"
+  bt_infra_cluster     = "ny2-aze-ntnx-12"
   bt_infra_network     = "ny2-autolab-db-ahv"
   os_version           = "rhel7"
   cpus                 = "2"
