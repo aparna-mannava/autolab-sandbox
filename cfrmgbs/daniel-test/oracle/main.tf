b4bd0ba7b3e2eef0a088f8b293686be87789344e
daniel.eliyahu@bottomline.com <daniel.eliyahu@bottomline.com>
2021-03-31 15:34:43 +0300

    fix hostaname

--- cfrmgbs/daniel-test/oracle/main.tf
+++ cfrmgbs/daniel-test/oracle/main.tf
@@ -16,7 +16,7 @@ locals {
       #firewall_group   = "CFRMRD_PR_ES" //"CFRMRD_PR_DB"
       hostgroup        = "BT CFRM CLOUD Oracle DB Servers"
       environment      = "feature_CFRMGC_621_bp_poc_create_vm_servers_on_saas_p" // 
-      hostname         = "us01vlcfoc19v1"
+      hostname         = "us01vlcfoc19v12"
     }
     datacenter = {
       name = "ny2"
@@ -27,7 +27,7 @@ locals {
 
 module "dblab_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "${local.facts.hostname}01-de" // gb00vlbpdb01-pr.saas-p.com
+  hostname             = "${local.facts.hostname}" // gb00vlbpdb01-pr.saas-p.com
   alias                = "${local.facts.bt_product}.${local.facts.bt_customer}.${local.facts.bt_tier}.${local.datacenter.id}.db02"// cfrmcloud.bp.prod.gb00.db01
   bt_infra_cluster     = "${local.facts.bt_infra_cluster}"
   bt_infra_network     = "${local.facts.bt_infra_network}"
