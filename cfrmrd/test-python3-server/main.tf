006b1d9c230021facc7dc7f068b8f8eb2ab234c9
Dor Weinstock <Dor.Weinstock@Bottomline.com>
2021-05-05 12:03:12 +0300

    fx

--- cfrmrd/test-python3-server/main.tf
+++ cfrmrd/test-python3-server/main.tf
@@ -7,22 +7,21 @@ locals {
       "bt_customer" = "cfrmrd"
       "bt_product"  = "cfrmrd"
       "bt_tier"     = "dev"
-      "bt_env"      = "test"
+      "bt_env"      = "autolab"
     }
     test_app_server_facts    = {
-      "bt_role" = "app"
+      "bt_role" = "python"
       "bt_customer" = "${local.facts.bt_customer}"
       "bt_product" = "${local.facts.bt_product}"
       "bt_tier" = "${local.facts.bt_tier}"
       "bt_env" = "${local.facts.bt_env}"
-      "bt_ic_mode" = "STANDALONE"
      }
 }
 
 module "test_python3_server" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlcfrmrd199"
-  alias                = "cfrmx-test-app-server"
+  hostname             = "us01vlcfrmrd399"
+  alias                = "cfrmx-python-server"
   bt_infra_network     = "ny2-autolab-app-ahv"
   bt_infra_cluster     = "ny2-aze-ntnx-12"
   cpus                 = "4"
@@ -30,7 +29,7 @@ module "test_python3_server" {
   os_version           = "rhel7"
   external_facts       = local.test_app_server_facts
   foreman_environment  = "feature_test_python3_server"
-  foreman_hostgroup    = "python3_server"
+  foreman_hostgroup    = "CFRMRD Python"
   lob                  = "CFRM"
   datacenter           = "ny2"
   additional_disks     = {
@@ -41,8 +40,8 @@ module "test_python3_server" {
 
 output "test_python3_server" {
   value = {
-    "fqdn"  = "${module.test_app_server.fqdn}",
-    "alias" = "${module.test_app_server.alias}",
-    "ip"    = "${module.test_app_server.ip}"
+    "fqdn"  = "${module.test_python3_server.fqdn}",
+    "alias" = "${module.test_python3_server.alias}",
+    "ip"    = "${module.test_python3_server.ip}"
   }
 }
