16d3fad15730514d97384c6f48c7370b6316322f
Rob Liberti <rliberti@bottomline.com>
2021-05-12 15:06:36 -0400

    PXDVOP-15446 test BT PMX ORA DB ERA

---
+++ pmx/auto1/oradbera/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  backend "s3" {}
+}
+
+locals {
+  product     = "pmx"
+  environment = "master"
+  datacenter  = "ny2"
+  facts       = {
+    "bt_tier"          = "auto"
+    "bt_env"           = "1"
+  }
+}
+
+module "pmx_amq_1" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vlpmxdbe99"
+  alias                = "${local.product}-${local.facts.bt_tier}${local.facts.bt_env}-odbe01"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = 2
+  memory               = 4096
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT PMX ORA DB ERA"
+  datacenter           = local.datacenter
+  lob                 = "PBS"
+  external_facts       = local.facts
+  additional_disks     = {
+    1                  = "50"
+    2                  = "25"
+    3                  = "25"
+  }
+}
+
+output "pmx_amq_1" {
+  value = {
+    "fqdn"  = "${module.pmx_amq_1.fqdn}",
+    "alias" = "${module.pmx_amq_1.alias}",
+    "ip"    = "${module.pmx_amq_1.ip}",
+  }
+}
