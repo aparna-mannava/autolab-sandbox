7402f64438344bbf8e365c901ad5a0257c0e5e8d
Eperkal <Eperkal@bottomline.com>
2021-05-13 19:16:59 +0300

    dirs app

--- eitantest/main.tf
+++ eitantest/main.tf
@@ -3,14 +3,10 @@ terraform {
 }
 #  Build test server
 locals {
-  product     = "cfrmit"
-  environment = "feature_CFRMGC_219_puppet_test" 
+ # product     = "cfrmcloud"
+ # environment = "feature_CFRMGC_219_puppet_test" 
+  environment = "feature_CFRMCLOUD_928_devops_puppet_add_additional_folders_to_be_created"
   hostname    = "us01"
-  facts = {
-    "bt_product"     = "cfrmiso"
-    "bt_role"     = "elastic"
-    "bt_host_number" = "001"
-  }
   datacenter = {
     name = "ny2"
     id   = "ny2"
@@ -18,14 +14,14 @@ locals {
 
   #|## Bitbucket server module configuration ########|#
   e001 = { 
-    hostname    = "${local.hostname}vlbb${local.facts.bt_host_number}"
-    alias       = "${local.hostname}vlbitbucket${local.facts.bt_host_number}"
+    hostname    = "${local.hostname}vlbb001"
+    alias       = "${local.hostname}vlbitbucket001"
     silo        = "autolab"
     hostgroup   = "BT CFRM Eitan Test" 
     facts       = {
-      "bt_product"  = "${local.facts.bt_product}"
-      "bt_role"     = "${local.facts.bt_role}"
-      "bt_app"      = "bitbucket"}
+      "bt_product"  = "cfrmcloud"
+      "bt_role" = "app"
+      "bt_tier" = "autolab"}
   }
 }
 module "e001" {
@@ -36,11 +32,11 @@ module "e001" {
   #bt_infra_cluster    = "il02-aza-ntnx-01"
   #bt_infra_network    = "il02_hosted_corp_app"
   ## auto.saas-n
-  bt_infra_cluster    = "ny5-aza-ntnx-14"
+  bt_infra_cluster    = "ny2-aze-ntnx-11"
   bt_infra_network    = "ny2-autolab-app-ahv"
   os_version          = "rhel7"
   cpus                = "4"
-  memory              = "8096"
+  memory              = "4048"
   lob                 = "CFRM"
   external_facts      = "${local.e001.facts}"
   foreman_environment = "${local.environment}"
@@ -56,6 +52,5 @@ output "e001" {
     "fqdn"  = "${module.e001.fqdn}",
     "alias" = "${module.e001.alias}",
     "ip"    = "${module.e001.ip}",
-    "app"   = "${local.e001.facts.bt_app}"
   }
 }
