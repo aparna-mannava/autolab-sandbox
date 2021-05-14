f69aa65250a3597d8babbbf38eb3e2df24b1a8ae
Rob Marke <rmarke@bottomline.com>
2021-05-14 09:34:42 +0100

    alias

--- rmarke/SelfService/1/main.tf
+++ rmarke/SelfService/1/main.tf
@@ -23,7 +23,7 @@ locals {
 module "adss001" {
   source              = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname            = "${local.adss001.hostname}"
-  alias               = "${local.product}-${local.datacenter.id}-ts-${local.facts.bt_role}101"
+  alias               = "${local.product}-${local.datacenter.name}-ts-${local.facts.bt_role}101"
   bt_infra_cluster    = "ny2-aza-ntnx-14"
   bt_infra_network    = "ny2-autolab-app-ahv"
   os_version          = "win2019"
