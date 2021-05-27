3021484e2cf4baf78af4f2b1f72cfed43afeb42b
Eperkal <Eperkal@bottomline.com>
2021-05-27 17:18:36 +0300

    c

--- eitantest/cfrm/main.tf
+++ eitantest/cfrm/main.tf
@@ -3,18 +3,14 @@ terraform {
 }
 #  Build test server
 locals {
- # product     = "cfrmcloud"
- # environment = "feature_CFRMGC_219_puppet_test" 
- # environment = "feature_CFRMCLOUD_928_devops_puppet_add_additional_folders_to_be_created"
   environment = "feature_CFRMCLOUD_131_cfrm_install_standalone"
-  hostname    = "us01"
   datacenter = {
     name = "ny2"
     id   = "ny2"
   }
 
-  hostname    = "${local.hostname}vlcfrm001"
-  alias       = "${local.hostname}vlbasecfrm001"
+  hostname    = "us01vlcfrm001"
+  alias       = "us01vlbasecfrm001"
   silo        = "autolab"
   hostgroup   = "BT CFRM Eitan Test"
   facts       = {
@@ -26,8 +22,8 @@ locals {
 
 module "cfrm001" {
   source              = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname            = "local.cfrm001.hostname"
-  alias               = "local.cfrm001.alias"
+  hostname            = local.hostname
+  alias               = local.alias
   ## saas-p NY2 on IL02 subnet
   #bt_infra_cluster    = "il02-aza-ntnx-01"
   #bt_infra_network    = "il02_hosted_corp_app"
