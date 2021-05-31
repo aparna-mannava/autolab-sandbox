79f4e4132aeeeea77b71f41ef4e151024ae56bdd
Eperkal <Eperkal@bottomline.com>
2021-05-31 11:29:11 +0300

    d

--- demo/demo1/main.tf
+++ demo/demo1/main.tf
@@ -28,8 +28,8 @@ locals {
 }
 module "demo1" {
   source              = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname            = "${local.demo1.hostname}"
-  alias               = "${local.demo1.alias}"
+  hostname            = "local.demo1.hostname"
+  alias               = "local.demo1.alias"
   ## saas-p NY2 on IL02 subnet
   #bt_infra_cluster    = "il02-aza-ntnx-01"
   #bt_infra_network    = "il02_hosted_corp_app"
@@ -40,10 +40,10 @@ module "demo1" {
   cpus                = "4"
   memory              = "8096"
   lob                 = "CFRM"
-  external_facts      = "${local.facts}"
-  foreman_environment = "${local.environment}"
-  foreman_hostgroup   = "${local.demo1.hostgroup}"
-  datacenter          = "${local.datacenter.name}"
+  external_facts      = "local.facts"
+  foreman_environment = "local.environment"
+  foreman_hostgroup   = "local.demo1.hostgroup"
+  datacenter          = "local.datacenter.name"
   additional_disks    = {
     1 = "100", // disk1 100gb 
   }
