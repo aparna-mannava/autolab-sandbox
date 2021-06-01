d5ad0f79c63b8f19166eaba775f2779478df7b7b
Shuki.Yulzari <shuki.yulzari@bottomline.com>
2021-06-01 16:15:05 +0300

    change code

--- cfrmiso/mgmt/autolab-test/main.tf
+++ cfrmiso/mgmt/autolab-test/main.tf
@@ -1,47 +1,50 @@
 terraform {
   backend "s3" {}
 }
- 
+
 locals {
     facts       = {
-      "bt_customer" = ""
-      "bt_product"  = "cfrmcloud"
+      bt_customer      = ""
+      bt_product       = "cfrmcloud"
+      bt_lob           = "cfrm"
+      bt_tier          = "autolab"
+      bt_env           = ""
+      bt_role          = "mgmt"
+      bt_infra_cluster = "ny5-azc-ntnx-16"  
+      bt_infra_network = "ny2-autolab-app-ahv"
+      hostgroup        = "BT CFRM CLOUD MGMT Base test"
+      environment      = "feature_CFRMCLOUD_824_cfrm_cloud_user_key"
+      hostname         = "us01vlcfrmmg951"       
     }
-    vm01facts    = {
-      "bt_tier" = "autolab"
-      "bt_role" = "mgmt"
-      "bt_env" = ""
-      "bt_customer" = local.facts.bt_customer
-      "bt_product" = local.facts.bt_product
-     }  
-} 
- 
-module "mgmt_951" {
+    datacenter = {
+      name = "ny2"
+      id   = "ny2"
+  }
+}
+
+module "nfstst_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlcfrmmg951"
-  alias                = "cfrmcloud-autolab-mgnf-clinet"
-  bt_infra_cluster     = "ny5-azc-ntnx-16"
-  bt_infra_network     = "ny2-autolab-app-ahv"
+  hostname             = local.facts.hostname 
+  alias                = "${local.facts.bt_product}.${local.facts.bt_tier}.${local.datacenter.id}.rmg3" // cfrmcloud.cfrm.auto.gb00.db01
+  bt_infra_cluster     = local.facts.bt_infra_cluster
+  bt_infra_network     = local.facts.bt_infra_network
+  lob                  = local.facts.bt_lob
+  foreman_environment  = local.facts.environment
+  foreman_hostgroup    = local.facts.hostgroup
+  datacenter           = local.datacenter.name
+  external_facts       = local.facts
   os_version           = "rhel7"
-  external_facts       = local.vm01facts
-  lob                  = "CFRM"
-  foreman_environment  = "feature_CFRMCLOUD_824_cfrm_cloud_user_key"
-  foreman_hostgroup    = "BT CFRM CLOUD MGMT Base test"
-  datacenter           = "ny2"
-  cpus                 = "2"
-  memory         	   = "4096"
-  additional_disks     = {
-    1 = "150", // Disk 1
+  cpus                 = "4"
+  memory         	     = "8096" 
+  additional_disks  = {
+    1 = "250"
   }
 } 
 
-  
-
-output "mgmt_951" {
+output "nfstst_1" {
   value = {
-    "fqdn"  = module.mgmt_951.fqdn,
-    "alias" = module.mgmt_951.alias,
-    "ip"    = module.mgmt_951.ip,
+    "fqdn"  = module.nfstst_1.fqdn,
+    "alias" = module.nfstst_1.alias,
+    "ip"    = module.nfstst_1.ip,
   }
-} 
-  
+}
