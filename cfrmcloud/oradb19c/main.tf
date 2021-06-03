b5eb7a3d85f38e5498c424d00ed8b22021df2694
Shuki.Yulzari <shuki.yulzari@bottomline.com>
2021-06-03 19:46:40 +0300

    s

--- cfrmcloud/oradb19c/main.tf
+++ cfrmcloud/oradb19c/main.tf
@@ -33,7 +33,7 @@ locals {
 
 module "dblab_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "${local.facts.hostname}lab01" //   gb00vlbpdb01-pr.saas-p.com
+  hostname             = "${local.facts.hostname}lab01" //   us01vlcfdblab01.auto.saas-n.com
   alias                = "${local.facts.bt_product}-${local.facts.bt_tier}-${local.datacenter.id}-oradb01"//cfrmcloud-autolab-ny2-oradb01
   bt_infra_cluster     = local.facts.bt_infra_cluster
   bt_infra_network     = local.facts.bt_infra_network
