c290573d663ddc5568870dc9e2dba1631acbaacd
Matt Zahorik <mzahorik@bottomline.com>
2021-06-08 17:34:21 -0400

    Build digital banking POC environment for Bolt testing

---
+++ mzahorik/banking-poc/main.tf
@@ -0,0 +1,192 @@
+terraform {
+  backend "s3" {}
+}
+
+locals {
+  environment = "master"
+  datacenter  = "ny2"
+  facts       = {
+    "bt_product"       = "dgb"
+    "bt_tier"          = "auto"
+  }
+}
+
+module "us01vldgbpoc1" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vldgbpoc1"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = 2
+  memory               = 1024
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = local.datacenter
+  lob                 = "DGB"
+  external_facts       = local.facts
+}
+
+module "us01vldgbpoc2" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vldgbpoc2"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = 2
+  memory               = 1024
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = local.datacenter
+  lob                 = "DGB"
+  external_facts       = local.facts
+}
+
+module "us01vldgbpoc3" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vldgbpoc3"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = 2
+  memory               = 1024
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = local.datacenter
+  lob                 = "DGB"
+  external_facts       = local.facts
+}
+
+module "us01vldgbpoc4" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vldgbpoc4"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = 2
+  memory               = 1024
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = local.datacenter
+  lob                 = "DGB"
+  external_facts       = local.facts
+}
+
+module "us01vldgbpoc5" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vldgbpoc5"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = 2
+  memory               = 1024
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = local.datacenter
+  lob                 = "DGB"
+  external_facts       = local.facts
+}
+
+module "us01vldgbpoc6" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vldgbpoc6"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = 2
+  memory               = 1024
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = local.datacenter
+  lob                 = "DGB"
+  external_facts       = local.facts
+}
+
+module "us01vldgbpoc7" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vldgbpoc7"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = 2
+  memory               = 1024
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = local.datacenter
+  lob                 = "DGB"
+  external_facts       = local.facts
+}
+
+module "us01vldgbpoc8" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vldgbpoc8"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = 2
+  memory               = 1024
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = local.datacenter
+  lob                 = "DGB"
+  external_facts       = local.facts
+}
+
+module "us01vldgbpoc9" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vldgbpoc9"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = 2
+  memory               = 1024
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = local.datacenter
+  lob                 = "DGB"
+  external_facts       = local.facts
+}
+
+module "us01vldgbpoc10" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vldgbpoc10"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = 2
+  memory               = 1024
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = local.datacenter
+  lob                 = "DGB"
+  external_facts       = local.facts
+}
+
+module "us01vldgbpoc11" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vldgbpoc11"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = 2
+  memory               = 1024
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = local.datacenter
+  lob                 = "DGB"
+  external_facts       = local.facts
+}
+
+module "us01vldgbpoc12" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vldgbpoc12"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = 2
+  memory               = 1024
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = local.datacenter
+  lob                 = "DGB"
+  external_facts       = local.facts
+}
