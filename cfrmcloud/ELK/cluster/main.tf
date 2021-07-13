b7a69c935d3d66a40c41070c7d78022ad4062e95
Shuki.Yulzari <shuki.yulzari@bottomline.com>
2021-07-13 16:01:48 +0300

    rebuild

--- cfrmcloud/ELK/cluster/main.tf
+++ cfrmcloud/ELK/cluster/main.tf
@@ -15,7 +15,7 @@ locals {
       bt_infra_network        = "ny2-autolab-app-ahv"
       #firewall_group          = "CFRMRD_PR_ES"
       hostgroup               = "CFRM BT CLOUD Elastic Servers"
-      environment             = "feature_CFRMCLOUD_1243_add_support_for_elk_5_6_2" //
+      environment             = "feature_CFRMCLOUD_1243_add_support_for_elk_5_6_2" ////
       bt_artemis_version      = "2.8.0"
       bt_es_version           = "5.6.2"
       bt_apacheds_version     = "2.0.0_M24"
