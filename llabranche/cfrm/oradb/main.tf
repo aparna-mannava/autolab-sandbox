e5a2376a4a8b18a7091cb5c60e13e3dbb26b5f0d
LaBranche, Laura <laura.labranche@bottomline.com>
2021-07-22 12:07:54 -0400

    adjust size

--- llabranche/cfrm/oradb/main.tf
+++ llabranche/cfrm/oradb/main.tf
@@ -19,8 +19,8 @@ module "ora_diag_1" {
   bt_infra_cluster     = "ny5-azc-ntnx-16"
   bt_infra_network     = "ny2-autolab-app-ahv"
   os_version           = "rhel7"
-  cpus                 = "2"
-  memory               = "4096"
+  cpus                 = "4"
+  memory               = "120 GiB"
   external_facts       = local.facts
   foreman_environment  = local.environment
   foreman_hostgroup    = "BT CFRM Oracle Server"
