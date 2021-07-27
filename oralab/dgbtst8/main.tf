89779e0c636689924d456bc998d0ace0110c8add
saumil.patel <saumil.patel@bottomline.com>
2021-07-27 12:19:13 +0530

    oem agent version check

--- oralab/dgbtst8/main.tf
+++ oralab/dgbtst8/main.tf
@@ -15,7 +15,7 @@ locals {
       bt_infra_network = "ny2-autolab-app-ahv"
       hostgroup        = "BT DGB Oradb Server"
       environment      = "feature_CLOUD_92713"
-      hostname         = "us01vldbtst22"
+      hostname         = "us01vldbtst24"
     }
     datacenter = {
       name = "ny2"
@@ -32,8 +32,8 @@ locals {
 
 module "dblab_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vldbtst22"
-  alias                = "${local.facts.bt_product}-${local.facts.bt_tier}-oralb22"
+  hostname             = "us01vldbtst24"
+  alias                = "${local.facts.bt_product}-${local.facts.bt_tier}-oralb24"
   bt_infra_cluster     = local.facts.bt_infra_cluster
   bt_infra_network     = local.facts.bt_infra_network
   lob                  = local.facts.bt_lob
