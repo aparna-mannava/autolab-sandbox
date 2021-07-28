550a32b85575b7689f5d61de1d58c88dd943a0e2
Vipul Vadher <Vipul.Vadher@bottomline.com>
2021-07-28 01:37:05 -0400

    changes

--- oralab/dgbtst10/main.tf
+++ oralab/dgbtst10/main.tf
@@ -13,7 +13,7 @@ locals {
       bt_infra_network = "ny2-autolab-app-ahv"
       hostgroup        = "BT DGB Oradb Server"
       environment      = "feature_CLOUD_92713"
-      hostname         = "us01vldbtst31"
+      hostname         = "us01vldbtst32"
     }
     datacenter = {
       name = "ny2"
@@ -29,8 +29,8 @@ locals {
 }
 module "dblab_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vldbtst31"
-  alias                = "${local.facts.bt_product}-${local.facts.bt_tier}-oralb31"
+  hostname             = "us01vldbtst32"
+  alias                = "${local.facts.bt_product}-${local.facts.bt_tier}-oralb32"
   bt_infra_cluster     = local.facts.bt_infra_cluster
   bt_infra_network     = local.facts.bt_infra_network
   lob                  = local.facts.bt_lob
