d1bf04715ee02eb5ac73f57e6becd3e2321074db
Shuki.Yulzari <shuki.yulzari@bottomline.com>
2021-07-30 13:06:20 +0300

    cfrm ha(fe+be) build

--- cfrmcloud/cfrm/ha/main.tf
+++ cfrmcloud/cfrm/ha/main.tf
@@ -9,29 +9,49 @@ locals {
       tier             = "autolab"
       env_id           = "01"
       bt_role          = "app"
+      cfrm_version     = "V590SP2"
       bt_infra_cluster = "ny5-azc-ntnx-16"
       bt_infra_network = "ny2-autolab-app-ahv"
       hostgroup        = "BT CFRM CLOUD Application Servers"
       firewall_group   = "CFRMRD_PPD_BE"
       environment      = "feature_CFRMCLOUD_1244_cfrm_automated_ha"
+      ic_host1         = "us01vlcoic1lb${local.facts.env_id}" ##us01vlcoic1pd01.saas-p.com
+      ic_host2         = "us01vlcoic2lb${local.facts.env_id}" ##us01vlcoic2pd01.saas-p.com
+      ic_host3         = "us01vlcobt3lb${local.facts.env_id}" ##us01vlcobt3pd01.saas-p.com
+      be_host1         = "us01vlcoae1lb${local.facts.env_id}" ##us01vlcoae1pd01.saas-p.com
+      be_host2         = "us01vlcoae2lb${local.facts.env_id}" ##us01vlcoae1pd02.saas-p.com
     }
-
-    cfrmfacts    = {
+    ## IC-FE
+    cfrmfacts_ic    = {
       bt_customer      = local.facts.customer
       bt_product       = local.facts.product
       bt_tier          = local.facts.tier
       bt_lob           = "cfrm"
       bt_env           = "ic"
       bt_env_id        = local.facts.env_id
-      bt_ic_version    = "V590SP2"
-      ## BE
-      ic_hostname1     = "us01vlcoic1lb${local.facts.env_id}" ##us01vlcoic1pd01.saas-p.com
-      ic_hostname2     = "us01vlcoic2lb${local.facts.env_id}" ##us01vlcoic2pd01.saas-p.com
-      ic_hostname3     = "us01vlcobt3lb${local.facts.env_id}" ##us01vlcobt3pd01.saas-p.com
-      be_hostname1     = "us01vlcoae1lb${local.facts.env_id}" ##us01vlcoae1pd01.saas-p.com
-      be_hostname2     = "us01vlcoae2lb${local.facts.env_id}" ##us01vlcoae1pd02.saas-p.com
+      bt_ic_version    = local.cfrm_version
+      ic_hostname1     = ic_host1
+      ic_hostname2     = ic_host2
+      ic_hostname3     = ic_host3
+      be_hostname1     = be_host1
+      be_hostname2     = be_host2
     }
+    ## AE-BE 
+    cfrmfacts_be    = {
+      bt_customer      = local.facts.customer
+      bt_product       = local.facts.product
+      bt_tier          = local.facts.tier
+      bt_lob           = "cfrm"
+      bt_env           = "be"
+      bt_env_id        = local.facts.env_id
+      bt_ic_version    = local.cfrm_version
+      ic_hostname1     = ic_host1
+      ic_hostname2     = ic_host2
+      ic_hostname3     = ic_host3
+      be_hostname1     = be_host1
+      be_hostname2     = be_host2
     }
+  }
     datacenter = {
       name = "ny2"
       id   = "ny2"
@@ -40,8 +60,8 @@ locals {
 
 module "chc-ic-be-lab01" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = local.cfrmfacts.be_hostname1 
-  alias                = "cfrm-cloud-chc-${local.facts.bt_tier}-${local.facts.env_id}-ny2-be01"
+  hostname             = local.cfrmfacts_be.be_hostname1 
+  alias                = "cfrm-cloud-chc-${local.cfrmfacts_be.bt_tier}-${local.facts.env_id}-${datacenter.id}-be01"
   bt_infra_network     = local.facts.bt_infra_network // 
   bt_infra_cluster     = local.facts.bt_infra_cluster
   foreman_environment  = local.facts.environment
@@ -52,7 +72,7 @@ module "chc-ic-be-lab01" {
   cpus                 = 8
   memory               = 32000
   os_version           = "rhel7"
-  external_facts       = local.cfrmfacts
+  external_facts       = local.cfrmfacts_be
   additional_disks     = {
     1 = "150"
   }
@@ -60,8 +80,8 @@ module "chc-ic-be-lab01" {
 
 module "chc-ic-be-lab02" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = local.cfrmfacts.be_hostname2
-  alias                = "cfrm-cloud-chc-${local.facts.bt_tier}-${local.facts.env_id}-ny2-be02"
+  hostname             = local.cfrmfacts_be.be_hostname2
+  alias                = "cfrm-cloud-chc-${local.cfrmfacts_be.bt_tier}-${local.facts.env_id}-${datacenter.id}-be02"
   bt_infra_network     = local.facts.bt_infra_network
   bt_infra_cluster     = local.facts.bt_infra_cluster
   foreman_environment  = local.facts.environment
@@ -72,7 +92,7 @@ module "chc-ic-be-lab02" {
   cpus                 = 8
   memory               = 32000
   os_version           = "rhel7"
-  external_facts       = local.cfrmfacts
+  external_facts       = local.cfrmfacts_be
   additional_disks     = {
     1 = "150"
   }
@@ -80,8 +100,8 @@ module "chc-ic-be-lab02" {
 
 module "chc-ic-fe-lab01" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = local.cfrmfacts.ic_hostname1
-  alias                = "cfrm-cloud-chc-${local.facts.bt_tier}-${local.facts.env_id}-ny2-ic01"
+  hostname             = local.cfrmfacts_ic.ic_hostname1
+  alias                = "cfrm-cloud-chc-${local.cfrmfacts_ic.bt_tier}-${local.facts.env_id}-${datacenter.id}-ic01"
   bt_infra_network     = local.facts.bt_infra_network
   bt_infra_cluster     = local.facts.bt_infra_cluster
   foreman_environment  = local.facts.environment
@@ -92,15 +112,15 @@ module "chc-ic-fe-lab01" {
   cpus                 = 8
   memory               = 16000
   os_version           = "rhel7"
-  external_facts       = local.cfrmfacts
+  external_facts       = local.cfrmfacts_ic
   additional_disks     = {
     1 = "150"
   }
 }
 module "chc-ic-fe-lab02" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = local.cfrmfacts.ic_hostname2
-  alias                = "cfrm-cloud-chc-${local.facts.bt_tier}-${local.facts.env_id}-ny2-ic02"
+  hostname             = local.cfrmfacts_ic.ic_hostname2
+  alias                = "cfrm-cloud-chc-${local.cfrmfacts_ic.bt_tier}-${local.facts.env_id}-${datacenter.id}-ic02"
   bt_infra_network     = local.facts.bt_infra_network
   bt_infra_cluster     = local.facts.bt_infra_cluster
   foreman_environment  = local.facts.environment
@@ -111,15 +131,15 @@ module "chc-ic-fe-lab02" {
   cpus                 = 8
   memory               = 16000
   os_version           = "rhel7"
-  external_facts       = local.cfrmfacts
+  external_facts       = local.cfrmfacts_ic
   additional_disks     = {
     1 = "150"
   }
 }
 module "chc-ic-fe-lab03" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = local.cfrmfacts.ic_hostname3
-  alias                = "cfrm-cloud-chc-${local.facts.bt_tier}-${local.facts.env_id}-ny2-btic03"
+  hostname             = local.cfrmfacts_ic.ic_hostname3
+  alias                = "cfrm-cloud-chc-${local.cfrmfacts_ic.bt_tier}-${local.facts.env_id}-${datacenter.id}-btic01"
   bt_infra_network     = local.facts.bt_infra_network
   bt_infra_cluster     = local.facts.bt_infra_cluster
   foreman_environment  = local.facts.environment
@@ -130,7 +150,7 @@ module "chc-ic-fe-lab03" {
   cpus                 = 4
   memory               = 8096
   os_version           = "rhel7"
-  external_facts       = local.cfrmfacts
+  external_facts       = local.cfrmfacts_ic
   additional_disks     = {
     1 = "150"
   }
