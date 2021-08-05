eb06ad05737633b447e063c517eb03f530af423e
Sunil Yadav <sunil.yadav@bottomline.com>
2021-08-05 21:11:06 +0530

    removed second vm

--- sunil.yadav/btiq/main.tf
+++ sunil.yadav/btiq/main.tf
@@ -28,38 +28,10 @@ module "datascience-vm" {
   }
   external_facts = local.facts
 }
-
-module "datascience-vm2" {
-  source              = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname            = "us01vlbtiqds007"
-  bt_infra_network    = "ny2-autolab-app-ahv"
-  bt_infra_cluster    = "ny5-azc-ntnx-16"
-  alias               = "btiq-${local.facts.bt_tier}${local.facts.bt_env}-ds07"
-  os_version          = "rhel8"
-  foreman_environment = "master"
-  foreman_hostgroup   = "BT Base Server"
-  datacenter          = "ny2"
-  lob                 = "btiq"
-  cpus                = "2"
-  memory              = "10240"
-  additional_disks = {
-    1 = "200"
-  }
-  external_facts = local.facts
-}
-
 output "datascience-vm" {
   value = {
     "fqdn"  = module.datascience-vm.fqdn,
     "alias" = module.datascience-vm.alias,
     "ip"    = module.datascience-vm.ip,
-  }
-}
-
-output "datascience-vm2" {
-  value = {
-    "fqdn"  = module.datascience-vm2.fqdn,
-    "alias" = module.datascience-vm2.alias,
-    "ip"    = module.datascience-vm2.ip,
   }
 }
