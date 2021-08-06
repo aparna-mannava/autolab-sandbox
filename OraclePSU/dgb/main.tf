341469016d236ba7ed886883a5ce8bcdd6908d51
Sanjay Mangroliya <sanjay.mangroliya@bottomline.com>
2021-08-06 09:27:10 -0400

    oracle db server for testing

--- OraclePSU/dgb/main.tf
+++ OraclePSU/dgb/main.tf
@@ -3,39 +3,60 @@ terraform {
 }
 
 locals {
-  product     = "dgb"
-  environment = "feature_CLOUD_91940"
-  datacenter  = "ny2"
-  facts       = {
-    "bt_tier" = "dev"
-    "bt_env"  = "1"
-    "bt_customer" = "fi8888"
+    facts       = {
+      bt_customer      = "fi1111"
+      bt_product       = "dgb"
+      bt_lob           = "CLOUD"
+      bt_tier          = "tst"
+      bt_env           = "2"
+      bt_role          = "oradb"
+      bt_infra_cluster = "ny5-azc-ntnx-16"
+      bt_infra_network = "ny2-autolab-app-ahv"
+      hostgroup        = "BT DGB Oradb Server"
+      environment      = "feature_CLOUD_91940"
+      hostname         = "us01vldbtst91"
+    }
+    datacenter = {
+      name = "ny2"
+      id   = "ny2"
   }
+    db01prod    = {
+      "bt_customer"     = local.facts.bt_customer
+      "bt_product"      = local.facts.bt_product
+      "bt_tier"         = local.facts.bt_tier
+      "bt_env"          = local.facts.bt_env
+      "bt_role"         = local.facts.bt_role
+     }
 }
 
-module "oradb_server_1" {
+module "dblab_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vltfrh9ora7"
-  bt_infra_cluster     = "ny2-aza-vmw-autolab"
-  bt_infra_network     = "ny2-autolab-db"
+  hostname             = "us01vldbtst91"
+  alias                = "${local.facts.bt_product}-${local.facts.bt_tier}-oralb09"
+  bt_infra_cluster     = local.facts.bt_infra_cluster
+  bt_infra_network     = local.facts.bt_infra_network
+  lob                  = local.facts.bt_lob
+  foreman_environment  = local.facts.environment
+  foreman_hostgroup    = local.facts.hostgroup
+  datacenter           = local.datacenter.name
+  external_facts       = local.db01prod
   os_version           = "rhel7"
-  cpus                 = "1"
+  cpus                 = "4"
   memory               = "4096"
-  lob                  = "CLOUD"
-  foreman_environment  = local.environment
-  foreman_hostgroup    = "BT DGB Oradb Server"
-  datacenter           = local.datacenter
-  external_facts       = local.facts
   additional_disks     = {
-    1 = "200",
-    2 = "50",
-    3 = "50"
+          1 = "200",
+	  2 = "200",
+	  3 = "200"
   }
 }
 
-output "oradb_server_1" {
+
+output "dblab_1" {
   value = {
-    "fqdn"  = "${module.oradb_server_1.fqdn}",
-    "ip"    = "${module.oradb_server_1.ip}",
+    "fqdn"  = module.dblab_1.fqdn,
+    "alias" = module.dblab_1.alias,
+    "ip"    = module.dblab_1.ip,
   }
 }
+
+
