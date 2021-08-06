4055d0e52f6310b379f50fd2027bf0654c9e9835
Sanjay Mangroliya <sanjay.mangroliya@bottomline.com>
2021-08-06 09:10:39 -0400

    oracle db server for testing

---
+++ OraclePSU/dgb/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  backend "http" {}
+}
+
+locals {
+  product     = "dgb"
+  environment = "feature_CLOUD_91940"
+  datacenter  = "ny2"
+  facts       = {
+    "bt_tier" = "dev"
+    "bt_env"  = "1"
+    "bt_customer" = "fi8888"
+  }
+}
+
+module "oradb_server_1" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vltfrh9ora7"
+  bt_infra_cluster     = "ny2-aza-vmw-autolab"
+  bt_infra_network     = "ny2-autolab-db"
+  os_version           = "rhel7"
+  cpus                 = "1"
+  memory               = "4096"
+  lob                  = "CLOUD"
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT DGB Oradb Server"
+  datacenter           = local.datacenter
+  external_facts       = local.facts
+  additional_disks     = {
+    1 = "200",
+    2 = "50",
+    3 = "50"
+  }
+}
+
+output "oradb_server_1" {
+  value = {
+    "fqdn"  = "${module.oradb_server_1.fqdn}",
+    "ip"    = "${module.oradb_server_1.ip}",
+  }
+}
