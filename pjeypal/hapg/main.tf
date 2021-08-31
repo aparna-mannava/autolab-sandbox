369d21b7a294ce874e985d6c6681fc49dd82fee3
Chris Drew <cdrew@bottomline.com>
2021-08-31 11:07:15 -0400

    added hap

--- pjeypal/hapg/main.tf
+++ pjeypal/hapg/main.tf
@@ -5,7 +5,7 @@ terraform {
 locals {
   etcd_servers    = ["us01vletcdfm01","us01vletcdfm02","us01vletcdfm03"]
   hapg_servers    = ["us01vlpgfm01","us01vlpgfm02","us01vlpgfm03"]
-  haproxy_server  = ["us01vlhapxfm01"]
+  haproxy_server  = ["us01vlhapxfm01","us01vlhapxfm02"]
   backrest_server = ["us01vlbkpfm01"]
   etcd_hosts_p    = ["'us01vletcdfm01.auto.saas-n.com','us01vletcdfm02.auto.saas-n.com','us01vletcdfm03.auto.saas-n.com'"]
   os              = "rhel7"
