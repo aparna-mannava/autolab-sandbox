078ca0267c965634b2869ccd6302d24b3b7695b9
Richard Grainger <richard.grainger@bottomline.com>
2021-10-08 13:14:50 +0100

    CEA-11622 Two more harbors

--- richard/harbor/main.tf
+++ richard/harbor/main.tf
@@ -10,8 +10,8 @@ module "harbor1" {
   os_version           = "rhel7"
   cpus                 = "4"
   datacenter           = "ny2"
-  foreman_environment  = "master"
-  foreman_hostgroup    = "BT Base Server"
+  foreman_environment  = "feature_CEA_11622_3"
+  foreman_hostgroup    = "BT CLOUD Harbor Server"
   lob                  = "CLOUD"
   memory               = "4096"
   additional_disks     = {
@@ -19,7 +19,29 @@ module "harbor1" {
   }
   external_facts       = {
     "bt_product" = "cloud"
-    "bt_role"    = "bt_base_server"
+    "bt_role"    = "harbor_server"
+    "bt_tier"    = "autolab"
+  }
+}
+
+module "harbor1b" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vlhrbr191"
+  bt_infra_cluster     = "ny2-aza-ntnx-07"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = "4"
+  datacenter           = "ny2"
+  foreman_environment  = "feature_CEA_11622_3"
+  foreman_hostgroup    = "BT CLOUD Harbor Server"
+  lob                  = "CLOUD"
+  memory               = "4096"
+  additional_disks     = {
+    1 = "500",
+  }
+  external_facts       = {
+    "bt_product" = "cloud"
+    "bt_role"    = "harbor_server"
     "bt_tier"    = "autolab"
   }
 }
@@ -32,8 +54,8 @@ module "harbor2" {
   os_version           = "rhel7"
   cpus                 = "4"
   datacenter           = "ny2"
-  foreman_environment  = "master"
-  foreman_hostgroup    = "BT Base Server"
+  foreman_environment  = "feature_CEA_11622_3"
+  foreman_hostgroup    = "BT CLOUD Harbor Server"
   lob                  = "CLOUD"
   memory               = "4096"
   additional_disks     = {
@@ -41,7 +63,29 @@ module "harbor2" {
   }
   external_facts       = {
     "bt_product" = "cloud"
-    "bt_role"    = "bt_base_server"
+    "bt_role"    = "harbor_server"
+    "bt_tier"    = "autolab"
+  }
+}
+
+module "harbor2b" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vlhrbr192"
+  bt_infra_cluster     = "ny2-aza-ntnx-07"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = "4"
+  datacenter           = "ny2"
+  foreman_environment  = "feature_CEA_11622_3"
+  foreman_hostgroup    = "BT CLOUD Harbor Server"
+  lob                  = "CLOUD"
+  memory               = "4096"
+  additional_disks     = {
+    1 = "500",
+  }
+  external_facts       = {
+    "bt_product" = "cloud"
+    "bt_role"    = "harbor_server"
     "bt_tier"    = "autolab"
   }
 }
