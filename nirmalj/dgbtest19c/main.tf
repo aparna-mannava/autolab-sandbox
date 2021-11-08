83de92dff096889dc07a980301f37b7e2c539ae9
Jani <nirmal.jani@bottomline.com>
2021-11-08 08:57:24 +0530

    creating new DGB 19c server

--- nirmalj/dgbtest19c/main.tf
+++ nirmalj/dgbtest19c/main.tf
@@ -28,7 +28,7 @@ module "cloud_dbserver_1" {
   memory               = "8192"
   foreman_environment  = local.environment
   lob                  = "CLOUD"
-  foreman_hostgroup    = "BT DGB Generic Server"
+  foreman_hostgroup    = "BT DGB Oradb 19c Server"
   datacenter           = local.datacenter
   external_facts       = local.facts
   additional_disks     = {
