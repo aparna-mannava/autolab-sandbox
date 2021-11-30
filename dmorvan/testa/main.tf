410e20cc5fd01b69d89e6d04b2bab079fc266588
dmorvan <dmorvan@bottomline.com>
2021-11-30 15:04:35 +0000

    TF test A

---
+++ dmorvan/testa/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {}
+}
+
+module "dmts01" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vldmts03"
+  alias                = "dmts-poc-03"
+  bt_infra_cluster     = "ny5-azc-ntnx-16"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  cpus                 = 2
+  lob                  = "CLOUD"
+  memory               = 2048
+  os_version           = "rhel8"
+  foreman_environment  = "nonprod"
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = "ny2"
+}
+
+output "dmts01" {
+  value = {
+    "fqdn"  = module.dmts01.fqdn,
+    "alias" = module.dmts01.alias,
+    "ip"    = module.dmts01.ip,
+  }
+}
