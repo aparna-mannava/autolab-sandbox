088ecc9b2efc843fa2122753a0dd0c7c134854b9
Delanoe, Yann <ydelanoe@bottomline.com>
2021-12-07 10:29:31 +0100

    FMDO-3243 : test jenkins new plugin deployment process

--- ydelanoe/jenkins/main.tf
+++ ydelanoe/jenkins/main.tf
@@ -20,6 +20,7 @@ locals {
 }
 
 module "fmcloud_jnk" {
+  lob                  = local.lob
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "us01vlfmjnk1"
   alias                = "fm-jnk01"
@@ -44,31 +45,3 @@ output "fmcloud_jnk" {
     "ip"    = "${module.fmcloud_jnk.ip}",
   }
 }
-
-
-/*
-module "test_data_1" {
-  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlfmtest002"
-  alias                = "fm-test01-d1"
-  bt_infra_cluster     = local.infra_cluster
-  bt_infra_network     = local.infra_network
-  os_version           = local.image
-  foreman_environment  = "master"
-  foreman_hostgroup    = local.hostgroup
-  datacenter           = local.datacenter
-  external_facts       = local.facts
-  cpus                 = "4"
-  memory               = "16384"
-  additional_disks     = {
-    1 = "150"
-  }
-}
-
-output "test_data_1" {
-  value = {
-    "fqdn"  = "${module.test_data_1.fqdn}",
-    "alias" = "${module.test_data_1.alias}",
-    "ip"    = "${module.test_data_1.ip}"
-  }
-}*/
