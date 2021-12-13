4035b81a24df48dcdae4e839440458120546390e
glevin <glevin@bottomline.com>
2021-12-13 11:38:13 +0200

    fx

--- genady/opensearchcluster/main.tf
+++ genady/opensearchcluster/main.tf
@@ -54,6 +54,26 @@ module "cfrm_opensearch_2" {
   } 
 }
 
+module "cfrm_opensearch_3" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vlcfrmx103"
+  alias                = "cfrm-opensearch-cluster3"
+  bt_infra_cluster     = "ny5-azc-ntnx-16"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = "2"
+  memory               = "8192"
+  foreman_environment  = "feature_CFRMX_7191_OpenSearch"
+  foreman_hostgroup    = "CFRMRD OpenSearch Cluster"
+  lob                  = "CFRM"
+  datacenter           = local.datacenter
+  external_facts       = local.facts
+  additional_disks     = {
+    1 = "500",
+    2 = "100",
+  } 
+}
+
 output "cfrm_opensearch_1" {
   value = {
     "fqdn"  = module.cfrm_opensearch_1.fqdn,
@@ -67,5 +87,12 @@ output "cfrm_opensearch_2" {
     "fqdn"  = module.cfrm_opensearch_2.fqdn,
     "alias" = module.cfrm_opensearch_2.alias,
     "ip"    = module.cfrm_opensearch_2.ip
+  }
+} 
+output "cfrm_opensearch_3" {
+  value = {
+    "fqdn"  = module.cfrm_opensearch_3.fqdn,
+    "alias" = module.cfrm_opensearch_3.alias,
+    "ip"    = module.cfrm_opensearch_3.ip
   }
 } 
