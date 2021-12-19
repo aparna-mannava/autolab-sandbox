7d028a83ae4cfde90ecc2d567737f31176290aa3
glevin <glevin@bottomline.com>
2021-12-19 15:58:07 +0200

    fx

--- genady/opensearch/main.tf
+++ genady/opensearch/main.tf
@@ -16,8 +16,8 @@ locals {
  
 module "cfrm_opensearch_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlcfrmx300"
-  alias                = "cfrm-opensearch-1"
+  hostname             = "us01vlcfrmx100"
+  alias                = "cfrm-opensearch1"
   bt_infra_cluster     = "ny5-aza-ntnx-19"
   bt_infra_network     = "ny2-autolab-app-ahv"
   os_version           = "rhel7"
@@ -33,6 +33,28 @@ module "cfrm_opensearch_1" {
     2 = "100",
   }
 }
+
+
+module "cfrm_opensearch_2" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vlcfrmx501"
+  alias                = "cfrm-opensearch2"
+  bt_infra_cluster     = "ny5-aza-ntnx-19"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
+  cpus                 = "2"
+  memory               = "8192"  
+  foreman_environment  = "feature_CFRMX_7191_Opensearch"
+  foreman_hostgroup    = "CFRMRD OpenSearch"
+  lob                  = "CFRM"
+  datacenter           = local.datacenter
+  external_facts       = local.facts
+  additional_disks     = {
+    1 = "500", 
+    2 = "100",
+  }
+}
+
 output "cfrm_opensearch_1" {
   value = { 
     "fqdn"  = module.cfrm_opensearch_1.fqdn,
