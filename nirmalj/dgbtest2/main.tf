9c8321613d91c7a8f29a1593de76fa1e09cc57be
Jani <nirmal.jani@bottomline.com>
2021-12-24 05:33:02 +0530

    re-building servers

--- nirmalj/dgbtest2/main.tf
+++ nirmalj/dgbtest2/main.tf
@@ -8,7 +8,7 @@ locals {
   environment = "master"
   datacenter  = "ny2"
   facts         = {
-    "bt_customer"         = "fi1234" #ex: fiXXXX
+    "bt_customer"         = "fi1234" #ex: fiXXXXXX
     "bt_tier"             = "sbx" #ex: sbx, tst, td, demo
     "bt_env"              = "" #ex: leave blank for first env, or non-zero-padded number
     "bt_product"          = "dgb"
