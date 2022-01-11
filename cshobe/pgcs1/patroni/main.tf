5a810710d22fd6f6225ef75253e8cf650d9cea07
Casey Shobe <Casey.Shobe@bottomline.com>
2022-01-10 18:43:54 -0600

    PG14 not supported, reduce to PG13

--- cshobe/pgcs1/patroni/main.tf
+++ cshobe/pgcs1/patroni/main.tf
@@ -37,7 +37,7 @@ locals {
     "bt_hapg_node2" = "${local.hapg_servers[1]}.${local.domain}"
     "bt_backup_node" = "${local.backrest_server[0]}.${local.domain}"
     "bt_cluster_name" = "pgcs1"
-    "bt_pg_version" = "14"
+    "bt_pg_version" = "13"
   }
 }
 
