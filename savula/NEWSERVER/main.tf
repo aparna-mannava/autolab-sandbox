327e18dc9e7a10092a1edb8bae6e75540b3385ce
Avula <suresh.avula@bottomline.com>
2022-01-15 17:06:50 -0500

    new srvr

---
+++ savula/NEWSERVER/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  backend "s3" {}
+}
+
+locals {
+  product        = "inf"
+  environment    = "master"
+  datacenter     = "ny2"
+  hostname       = "us01vwsqlv2019"
+  hostgroup      = "BT MSSQL 2019 Server"
+  facts          = {
+    "bt_env"          = "1"
+    "bt_product"      = "inf"
+    "bt_tier"         = "dev"
+    "bt_role"         = "mssql"
+  }
+}
+
+module "sql_103479_1" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "${local.hostname}"
+  alias                = ""
+  bt_infra_cluster     = "ny2-azb-ntnx-08"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  lob                  = "CLOUD"
+  os_version           = "win2019"
+  cpus                 = "4"
+  memory               = "16384"
+  external_facts       = "${local.facts}"
+  foreman_environment  = "${local.environment}"
+  foreman_hostgroup    = "${local.hostgroup}"
+  datacenter           = "${local.datacenter}"
+  additional_disks     = {
+    1 = "200",
+    2 = "100",
+    3 = "50",
+    4 = "50",
+    5 = "50",
+    6 = "50"
+  }
+}
+
+output "sql_103479_1" {
+  value = {
+    "fqdn"  = "${module.sql_103479_1.fqdn}",
+    "alias" = "${module.sql_103479_1.alias}",
+    "ip"    = "${module.sql_103479_1.ip}",
+  }
+}
