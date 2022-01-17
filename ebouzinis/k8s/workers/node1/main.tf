7fa7b676674041ea2ede91d6ad919b7a474851a5
Evangelos Bouzinis <Evangelos.Bouzinis@bottomline.com>
2022-01-17 13:55:59 +0000

    K8s Cluster for testing

---
+++ ebouzinis/k8s/workers/node1/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {}
+}
+
+module "dmts03" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01k8st3stn0de1"
+  alias                = "k8s-poc-n0de1"
+  bt_infra_cluster     = "ny5-azc-ntnx-16"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  cpus                 = 6
+  lob                  = "CLOUD"
+  memory               = 16000
+  os_version           = "rhel8"
+  foreman_environment  = "nonprod"
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = "ny2"
+}
+
+output "dmts03" {
+  value = {
+    "fqdn"  = module.dmts03.fqdn,
+    "alias" = module.dmts03.alias,
+    "ip"    = module.dmts03.ip,
+  }
+}
