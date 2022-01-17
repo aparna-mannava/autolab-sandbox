5faca93cd35421e34936a0731fb43caaa391bef3
Evangelos Bouzinis <Evangelos.Bouzinis@bottomline.com>
2022-01-17 15:37:15 +0000

    K8s Cluster

--- ebouzinis/k8s/workers/node3/main.tf
+++ ebouzinis/k8s/workers/node3/main.tf
@@ -4,13 +4,13 @@ terraform {
 #Destroy1
 module "dmts03" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlebtest01"
-  alias                = "ebtest-poc-03"
+  hostname             = "us01vlk8sn0de3"
+  alias                = "k8s-poc-n0de3"
   bt_infra_cluster     = "ny5-azc-ntnx-16"
   bt_infra_network     = "ny2-autolab-app-ahv"
-  cpus                 = 2
+  cpus                 = 8
   lob                  = "CLOUD"
-  memory               = 2048
+  memory               = 4000
   os_version           = "rhel8"
   foreman_environment  = "nonprod"
   foreman_hostgroup    = "BT Base Server"
