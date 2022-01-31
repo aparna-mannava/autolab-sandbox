cdb0435a59630dc8d1e8b0f1151142eab8081b4b
Jayapal <Jaycee.Jayapal@bottomline.com>
2022-01-31 08:51:33 -0600

    changes

--- jaycee/dev1/main.tf
+++ jaycee/dev1/main.tf
@@ -16,7 +16,7 @@ module "app_server_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "us01vltferin03"
   alias                = "${local.product} -${local.facts.bt_tier}${local.facts.bt_env}-app01"
-  bt_infra_network     = "ny2-autolab-app"
+  bt_infra_network     ="ny2-autolab-app-ahv"
   bt_infra_cluster     = "ny2-aze-ntnx-11"
   os_version           = "rhel7"
   cpus                 = "4"
