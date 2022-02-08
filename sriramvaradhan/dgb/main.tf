8fbaa93dbc0f5373b6a8bacf70ad013a8e71c3db
Varadhan <Sriram.Varadhan@bottomline.com>
2022-02-08 15:33:56 -0500

    provision ora12

--- sriramvaradhan/dgb/main.tf
+++ sriramvaradhan/dgb/main.tf
@@ -8,8 +8,8 @@ locals {
   environment = "master"
   datacenter  = "ny2"
   facts         = {
-    "bt_customer"         = "fi7888" #ex: fiXXXX
-    "bt_tier"             = "pr" #ex: sbx, tst, td, demo
+    "bt_customer"         = "fi8877" #ex: fiXXXX
+    "bt_tier"             = "sbx" #ex: sbx, tst, td, demo
     "bt_env"              = "1" #ex: leave blank for first env, or non-zero-padded number
     "bt_product"          = "dgb"
     "bt_product_version"  = "3.6"
@@ -19,7 +19,7 @@ locals {
 
 module "cloud_dbserver_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vldgbd7888"
+  hostname             = "us01vldgbd8877"
   alias                = "${local.lob}-${local.facts.bt_tier}${local.facts.bt_env}-${local.facts.bt_customer}-dba"
   bt_infra_cluster     = "ny5-aza-ntnx-14"
   bt_infra_network     = "ny2-autolab-app-ahv"
@@ -33,8 +33,8 @@ module "cloud_dbserver_1" {
   external_facts       = local.facts
   additional_disks     = {
     1 = "300",
-    2 = "50",
-    3 = "50"
+    2 = "25",
+    3 = "25"
   }
 }
 
