56451570aab39e2b8e27e75b1698be6412783e8e
Jani <nirmal.jani@bottomline.com>
2022-02-09 21:07:14 +0530

    creating new dgb test server

--- nirmalj/dgb19c1/main.tf
+++ nirmalj/dgb19c1/main.tf
@@ -8,7 +8,7 @@ locals {
   environment = "feature_CLOUD_103960"
   datacenter  = "ny2"
   facts         = {
-    "bt_customer"         = "fi7890" #ex: fiXXX
+    "bt_customer"         = "fi7890" #ex: fiXXXXX
     "bt_tier"             = "sbx" #ex: sbx, tst, td, demo
     "bt_env"              = "" #ex: leave blank for first env, or non-zero-padded number
     "bt_product"          = "dgb"
