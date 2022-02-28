67bbf47a92b1273ed3896d5d8f922f405f444703
Evangelos Bouzinis <Evangelos.Bouzinis@bottomline.com>
2022-02-28 10:33:05 +0000

    changes

--- ebouzinis/dev/main.tf
+++ ebouzinis/dev/main.tf
@@ -14,7 +14,7 @@ locals {
 
 module "ebouzinis-dev-machine" {
   source              = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
-  hostname            = "us01v1bouz1"
+  hostname            = "us01v1ebdev1"
   alias               = "ebouzinis-dev-1"
   bt_infra_cluster    = "ny5-azc-ntnx-16"
   bt_infra_network    = "ny2-autolab-app-ahv"
