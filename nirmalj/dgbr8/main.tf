17d513c2b6f2d4bd9944b3e79c55a0c6fcad6ed9
Jani <nirmal.jani@bottomline.com>
2022-03-04 18:56:33 +0530

    creating rhel8 base server

--- nirmalj/dgbr8/main.tf
+++ nirmalj/dgbr8/main.tf
@@ -8,7 +8,7 @@ locals {
   environment = "master"
   datacenter  = "ny2"
   facts         = {
-    "bt_customer"         = "fi9999" #ex: fiXXXX
+    "bt_customer"         = "fi9999" #ex: fiXXXXXXX
     "bt_tier"             = "pr" #ex: sbx, tst, td, demo
     "bt_env"              = "1" #ex: leave blank for first env, or non-zero-padded number
     "bt_product"          = "dgb"
@@ -21,7 +21,7 @@ module "cloud_dbserver_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "us01vldgbd9999"
   alias                = "${local.lob}-${local.facts.bt_tier}${local.facts.bt_env}-${local.facts.bt_customer}-dba"
-  bt_infra_cluster     = "ny5-aza-ntnx-14"
+  bt_infra_cluster     = "ny5-azc-ntnx-16"
   bt_infra_network     = "ny2-autolab-app-ahv"
   os_version           = "rhel8"
   cpus                 = "4"
