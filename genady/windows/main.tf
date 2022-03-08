739c454958d5683dc47f30e0aad2d011bf7a023b
glevin <glevin@bottomline.com>
2022-03-08 15:53:17 +0200

    fx

--- genady/windows/main.tf
+++ genady/windows/main.tf
@@ -12,7 +12,7 @@ locals {
     "bt_env"             = "staging"
     "bt_role"            = "windows"
   }
-}
+} 
    
 module "cfrm_windows_1" { 
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
