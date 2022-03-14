59722ac31ba55cbb268bc26881a67cad2cff31e9
glevin <glevin@bottomline.com>
2022-03-14 11:45:52 +0200

    fx

--- genady/standalone/main.tf
+++ genady/standalone/main.tf
@@ -18,7 +18,7 @@ module "cfrm_opensearch_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "us01vlcfrmx888"
   alias                = "cfrm-opensearch-standalone"
-  bt_infra_cluster     = "ny5-azc-ntnx-16"
+  bt_infra_cluster     = "ny2-azb-ntnx-08"
   bt_infra_network     = "ny2-autolab-app-ahv"
   os_version           = "rhel8"  
   cpus                 = "2"
