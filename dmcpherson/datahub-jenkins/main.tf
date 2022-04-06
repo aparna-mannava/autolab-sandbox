422cb38a5055d4b6ae8159a41e79c66b60222eac
Dave McPherson <dmcpherson@bottomline.com>
2022-04-06 13:42:52 -0400

    Testing puppet changes for datahub group

---
+++ dmcpherson/datahub-jenkins/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  backend "s3" {}
+}
+
+locals {
+  product     = "cfrm"
+  environment = "feature/CUT1-14468-provide-access-to-cfrm-bi-team-to-the-datahub-server"
+  datacenter  = "ny2"
+  facts       = {
+    "bt_tier" = "dev"
+    "bt_product" = "cfrm"
+    "bt_env"  = "dev"
+    "bt_customer" = "datahub"
+    "bt_manage_plugins" = "NO"
+    "bt_jenkins_version" = "2.207"
+    "bt_role" = "jenkins"
+  }
+}
+
+module "jenkins_server_1" {
+  source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vljkns00002"
+  alias                = "cfrm-datahub-prod-jkns01"
+  bt_infra_network     = "ny2-autolab-app"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  os_version           = "rhel7"
+  cpus                 = "4"
+  memory               = "4096"
+  foreman_environment  = local.environment
+  foreman_hostgroup    = "BT CFRM Jenkins Active Server"
+  datacenter           = local.datacenter
+  external_facts       = local.facts
+  additional_disks     = {
+    1 = "300",
+    2 = "150"
+  }
+}
+
+output "jenkins_server_1" {
+  value = {
+    "fqdn"  = module.jenkins_server_1.fqdn,
+    "alias" = module.jenkins_server_1.alias,
+    "ip"    = module.jenkins_server_1.ip,
+  }
+}
