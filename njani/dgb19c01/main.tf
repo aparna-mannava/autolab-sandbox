9861a030637c294eb749254f5b6685f1dd5bba9f
Jani <nirmal.jani@bottomline.com>
2022-07-29 12:46:57 +0530

    adding new server

--- njani/dgb19c01/main.tf
+++ njani/dgb19c01/main.tf
@@ -5,7 +5,7 @@ terraform {
 locals {
   lob         = "dgb"
   product     = "dgb"
-  environment = "feature_CLOUD_109889"
+  environment = "master"
   datacenter  = "ny2"
   facts         = {
     "bt_customer"         = "fi9999" #ex: fiXXXX
