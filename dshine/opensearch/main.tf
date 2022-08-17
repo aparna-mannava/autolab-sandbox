ca65ac9460199ae0736912d55d3b99206d3ce79f
Shine.Daniel <Daniel.Shine@bottomline.com>
2022-08-17 14:38:53 +0300

    change vm

--- dshine/opensearch/main.tf
+++ dshine/opensearch/main.tf
@@ -12,7 +12,7 @@ locals {
       bt_env             = "03"         // mandatory
       bt_role            = "opensearch"       // mandatory
       bt_infra_network   = "ny2-autolab-app-ahv" //
-      bt_infra_cluster   = "ch07-azb-ntnx-02"
+      bt_infra_cluster   = "ny5-aze-ntnx-21"
       hostgroup          = "BT Base Server" // Foreman hostgroup for BT base servers in Autolab.saas-n domain
       environment        = "master" // Bitbucket Puppet controlrepo branch name
       hostname           = "us01vlcfrm"
