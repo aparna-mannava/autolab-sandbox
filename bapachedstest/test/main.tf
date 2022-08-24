f42b78a7b9a4b91ee00c88026c6cc838e083b2b8
Prudhvi.Vuppalapati <Prudhvi.Vuppalapati@bottomline.com>
2022-08-24 11:58:11 +0530

    Add apacheds VM

--- bapachedstest/test/main.tf
+++ bapachedstest/test/main.tf
@@ -7,7 +7,7 @@ locals {
       "bt_customer" = "cfrmrd"
       "bt_product"  = "cfrmrd"
       "bt_tier"     = "dev"
-      "bt_role"     = "apachedsz"
+      "bt_role"     = "apacheds"
     }
     devops1_apacheds_facts    = {
       "bt_env"      = "devops1"
@@ -21,16 +21,16 @@ locals {
  
 module "devops1_apacheds" {
   source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlcfrmrdxz"
-  alias                = "cfrmx-apachedsz"
+  hostname             = "us01vlcfrmrdx1"
+  alias                = "cfrmx-apachedss"
   bt_infra_network     = "ny2-autolab-app-ahv"
-  bt_infra_cluster     = "ny2-aze-ntnx-12"
+  bt_infra_cluster     = "ny2-azd-ntnx-10"
   cpus                 = "2"
   memory               = "8192"
   os_version           = "rhel7"
   external_facts       = local.devops1_apacheds_facts
   foreman_environment  = "feature_CFRMRD_8000"
-  foreman_hostgroup    = "CFRMRD apachedsz"
+  foreman_hostgroup    = "CFRMRD apacheds"
   lob                  = "CFRM"
   datacenter           = "ny2"
   additional_disks     = {
