5cb7b888f0069914273df7cf185395543ef352b2
Prudhvi.Vuppalapati <Prudhvi.Vuppalapati@bottomline.com>
2022-08-24 11:07:35 +0530

    Add apacheds VM

--- bapachedstest/test/main.tf
+++ bapachedstest/test/main.tf
@@ -21,7 +21,7 @@ locals {
  
 module "devops1_apacheds" {
   source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlcfrmrdxz"
+  hostname             = "us01vlcfrmrdxx"
   alias                = "cfrmx-apachedsz"
   bt_infra_network     = "ny2-autolab-app-ahv"
   bt_infra_cluster     = "ny2-aze-ntnx-12"
