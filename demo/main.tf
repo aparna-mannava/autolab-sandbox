b54fc7842668ba592d7a6da16b219a173b9f0be4
hitesh divte <hitesh.divte@us01vlinfradev2.auto.saas-n.com>
2022-08-25 16:45:01 -0400

    TEST VM

--- demo/main.tf
+++ demo/main.tf
@@ -2,40 +2,38 @@ terraform {
   backend "s3" {}
 }
 
-
 locals {
-  product        = "inf"
-  environment    = "master"
-  datacenter     = "ny2"
-  hostname       = "us01vlntnxtst1"
-  hostgroup      = "BT Base Server"
-  facts          = {
-    "bt_product" = "inf"
-    "bt_tier"    = "auto"
+  environment = "master"
+  datacenter  = "ny2"
+  facts         = {
+    "bt_tier"             = "pr"
+    "bt_env"              = "1"
+    "bt_product"          = "inf"
+    "bt_role"             = "base"
   }
 }
- 
 
 module "inf_test_1" {
   source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
-  hostname             = "${local.hostname}"
+  hostname             = "us01vlntnxtst1"
   alias                = "inf-auto-tst01"
-  bt_infra_environment = "ny2-autolab-app"
-  lob                  = "inf"
-  os_version           = "rhel6"
+  bt_infra_cluster     = "ny2-aze-ntnx-12"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel7"
   cpus                 = "2"
-  memory               = "4096"
-  external_facts       = "${local.facts}"
-  foreman_environment  = "${local.environment}"
-  foreman_hostgroup    = "${local.hostgroup}"
-  datacenter           = "${local.datacenter}"
+  memory               = "8192"
+  foreman_environment  = local.environment
+  lob                  = "CLOUD"
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = local.datacenter
+  external_facts       = local.facts
 }
 
- 
+
 output "inf_test_1" {
   value = {
-    "fqdn"  = "${module.inf_test_1.fqdn}",
-    "alias" = "${module.inf_test_1.alias}",
-    "ip"    = "${module.inf_test_1.ip}",
+    "fqdn"  = module.inf_test_1.fqdn,
+    "alias" = module.inf_test_1.alias,
+    "ip"    = module.inf_test_1.ip,
   }
 }
