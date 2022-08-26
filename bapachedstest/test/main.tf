cf330603d2c4031611741e686b1463115c339b7d
Prudhvi.Vuppalapati <Prudhvi.Vuppalapati@bottomline.com>
2022-08-26 15:32:53 +0530

    Add apacheds role

--- bapachedstest/test/main.tf
+++ bapachedstest/test/main.tf
@@ -21,8 +21,8 @@ locals {
  
 module "devops1_apacheds" {
   source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlcfrmrd18"
-  alias                = "cfrmx-apacheds3"
+  hostname             = "us01vlcfrmrd19"
+  alias                = "cfrmx-apacheds4"
   bt_infra_network     = "ny2-autolab-app-ahv"
   bt_infra_cluster     = "ny5-aza-ntnx-19"
   cpus                 = "2"
