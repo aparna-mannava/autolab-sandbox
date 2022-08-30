4e389315cc745f83e3bbc5cb314e0e2fc6eb2d97
Prudhvi.Vuppalapati <Prudhvi.Vuppalapati@bottomline.com>
2022-08-30 12:34:27 +0530

    Add oracledb configuration

--- oracledb/test/main.tf
+++ oracledb/test/main.tf
@@ -21,8 +21,8 @@ locals {
  
 module "devops1_apacheds" {
   source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlcfrmrdx4"
-  alias                = "cfrmx-oracledb3"
+  hostname             = "us01vlcfrmrdx5"
+  alias                = "cfrmx-oracledb4"
   bt_infra_network     = "ny2-autolab-app-ahv"
   bt_infra_cluster     = "ny5-aza-ntnx-19"
   cpus                 = "2"
