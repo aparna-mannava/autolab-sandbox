410792684fdf57282c4894185ebe0678044e3743
Prudhvi.Vuppalapati <Prudhvi.Vuppalapati@bottomline.com>
2022-09-14 17:45:55 +0530

    Add apacheds vm

--- oracle/test/main.tf
+++ oracle/test/main.tf
@@ -21,8 +21,8 @@ locals {
  
 module "devops1_apacheds" {
   source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlcfrmrdx13"
-  alias                = "cfrmx-oracledb12"
+  hostname             = "us01vlcfrmrdx14"
+  alias                = "cfrmx-oracledb13"
   bt_infra_network     = "ny2-autolab-app-ahv"
   bt_infra_cluster     = "ny5-aza-ntnx-19"
   cpus                 = "2"
