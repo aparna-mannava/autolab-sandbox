d22cbe4f63ac8da3cf945121c095c24fb44f661c
Yevhen Surykin <yevhen.surykin@bottomline.com>
2022-09-14 10:27:50 +0300

    Adding new ysurykin-oradb projects 3

--- ysurykin/oradb/main.tf
+++ ysurykin/oradb/main.tf
@@ -1,3 +1,7 @@
+terraform {
+  backend "http" {}
+}
+
 module "app_server_1" {
   source               = "git::https://us-pr-stash.saas-p.com/scm/trrfrm/terraform-module-infrastructure.git?ref=master"
   hostname             = "us01vlsndbxoradb01"
