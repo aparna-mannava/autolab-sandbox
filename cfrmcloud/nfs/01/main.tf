f3197e9ea4d3a264e7615d3d3375bf01992f2e0c
Maxim Akimov <makimov@bottomline.com>
2022-09-28 08:52:18 +0300

    CFRMCLOUD-2547 try another bt_infra_cluster

--- cfrmcloud/nfs/01/main.tf
+++ cfrmcloud/nfs/01/main.tf
@@ -11,7 +11,7 @@ locals {
       bt_env           = "01"
       bt_role          = "mgmt"
       bt_infra_network = "ny2-autolab-app-ahv"
-      bt_infra_cluster = "ny5-aze-ntnx-21"
+      bt_infra_cluster = "ny5-aza-ntnx-19"
       hostgroup        = "BT CFRM CLOUD MGMT Base"
       environment      = "feature_CFRMCLOUD_2547_manage_cfrmcloud_related_users_authorized_keys" //
       hostname         = "us01vlcfrmnfs"
@@ -33,7 +33,7 @@ locals {
 module "nfsdev_autolab_1" {
   source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
   hostname             = "${local.facts.hostname}01" //    us01vlcfrmnfs.autolab.saas-n.com
-  alias                = "${local.facts.bt_product}-${local.datacenter.id}-dev-nfs01"//cfrmcloud-ny2-dev-nfs01.autolab.saas-n.com
+  alias                = "${local.facts.bt_product}-${local.datacenter.id}-dev-nfs01" //cfrmcloud-ny2-dev-nfs01.autolab.saas-n.com
   bt_infra_cluster     = local.facts.bt_infra_cluster
   bt_infra_network     = local.facts.bt_infra_network
   //firewall_group       = local.facts.firewall_group
