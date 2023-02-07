626236dee28ce269a742f1c8304dbabc37d3f5e4
Prudhvi.Vuppalapati <Prudhvi.Vuppalapati@bottomline.com>
2023-02-07 16:10:07 +0530

    add new VM

--- jenkins/test/main.tf
+++ jenkins/test/main.tf
@@ -3,46 +3,45 @@ terraform {
 }
 
 locals {
-    facts       = {
-      "bt_customer" = "cfrmrd"
-      "bt_product"  = "cfrmrd"
-      "bt_tier"     = "dev"
-      "bt_role"     = "jenkins"
-    }
-    staging_jenkins_facts    = {
-      "bt_env"      = "devops"
-      "bt_role"     = local.facts.bt_role
-      "bt_customer" = local.facts.bt_customer
-      "bt_product"  = local.facts.bt_product
-      "bt_tier"     = local.facts.bt_tier
-     
-     }    
+  facts       = {
+    "bt_customer" = "cfrmrd"
+    "bt_product" = "cfrmrd"
+    "bt_role" = "standalone"  
+    "bt_tier" = "dev"
+    "bt_env" = "dynamic-scan"
+  }
+  app_server_1_facts   = {
+    "bt_role" = local.facts.bt_role
+    "bt_customer" = local.facts.bt_customer
+    "bt_product" = local.facts.bt_product
+    "bt_tier" = local.facts.bt_tier
+    "bt_env" = local.facts.bt_env
+  }
 }
- 
-module "staging_jenkins" {
+
+module "app_server_1" {
   source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlcfrmrdjnkss"
-  alias                = "cfrmrd-jenkinss"
+  hostname             = "us01vlcfrmrd1188"
+  alias                = "dynamic-scan-standalones"
   bt_infra_network     = "ny2-dgb-development"
-  bt_infra_cluster     = "ny2-azd-ntnx-10"
-  cpus                 = "4"
-  memory               = "8192"
+  bt_infra_cluster     = "ny5-azc-ntnx-16"
+  cpus                 = 6
+  memory               = 24000
   os_version           = "rhel7"
-  external_facts       = local.staging_jenkins_facts
-  foreman_environment  = "feature_CFRMRD_37555"
-  foreman_hostgroup    = "BT CFRMRD Jenkins"
-  lob                  = "CFRM"
+  foreman_environment  = "feature_CFRMRD_37888"
+  foreman_hostgroup    = "CFRMRD OpenSearch"
   datacenter           = "ny2"
+  external_facts       = local.app_server_1_facts
   additional_disks     = {
-    1 = "50",
+    1 = "100",
 	2 = "100"
   }
 }
 
-output "staging_jenkins" {
+output "app_server_1" {
   value = {
-    "fqdn"  = module.staging_jenkins.fqdn,
-    "alias" = module.staging_jenkins.alias,
-    "ip"    = module.staging_jenkins.ip,
+    "fqdn"  = module.app_server_1.fqdn,
+    "alias" = module.app_server_1.alias,
+    "ip"    = module.app_server_1.ip,
   }
-} 
+}
