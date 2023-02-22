5d04b1acdb6235221327a20c26f3e41b2bc6bda7
Jani <nirmal.jani@bottomline.com>
2023-02-22 10:54:20 -0500

    changing environment to master

--- njani/ora19cr8/main.tf
+++ njani/ora19cr8/main.tf
@@ -5,7 +5,7 @@ terraform {
 locals {
   lob         = "dgb"
   product     = "dgb"
-  environment = "feature_CLOUD_115675"
+  environment = "master"
   datacenter  = "ny2"
   facts         = {
     "bt_customer"         = "fi9090" #ex: fiXXXX
