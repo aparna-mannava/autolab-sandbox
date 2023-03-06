73a39b69e43a65d6337d3e06138e65e2e4621772
prabhanjan.desai <Prabhanjan.desai@bottomline.com>
2023-03-06 16:59:47 +0530

    testing_beat

---
+++ prabhanjan/main.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  backend "s3" {}
+}
+​
+module "rhel9srea1" {
+source              = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
+hostname            = "us01vlr9srea2"
+bt_infra_cluster    = "ny5-azf-ntnx-24"
+bt_infra_network    = "ny2-autolab-app-ahv"
+cpus                = 2
+lob                 = "CLOUD"
+memory              = 2048
+os_version          = "rhel9"
+foreman_environment = "master"
+foreman_hostgroup   = "BT Base Server"
+datacenter          = "ny2"
+}
+​
+output "rhel9srea1" {
+value = {
+"fqdn"  = module.rhel9srea1.fqdn,
+"alias" = module.rhel9srea1.alias,
+"ip"    = module.rhel9srea1.ip,
+}
+}
