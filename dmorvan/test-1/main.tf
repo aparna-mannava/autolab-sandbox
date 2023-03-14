b9e240d1994ec961143faa37a7dceb7c50f35400
Dave Morvan <dmorvan@bottomline.com>
2023-03-14 11:08:56 +0000

    increase mem 4GB & source

--- dmorvan/test-1/main.tf
+++ dmorvan/test-1/main.tf
@@ -14,14 +14,14 @@ locals {
 }
 
 module "dmts05" {
-  source              = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
+  source              = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=feature/CLOUD-119870"
   hostname            = "us01vldmts05"
   alias               = "dmts-ts-05"
   bt_infra_cluster    = "ny5-aza-ntnx-14"
   bt_infra_network    = "ny2-autolab-app-ahv"
   cpus                = 2
   lob                 = "CLOUD"
-  memory              = 2048
+  memory              = 4096
   os_version          = "rhel9"
   external_facts      = local.facts
   foreman_environment = local.environment
