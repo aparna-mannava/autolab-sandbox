014d3e8159060f4934d5e5df88f6b0828d2006e8
Nirmal Jani <nirmal.jani@bottomline.com>
2023-04-05 18:57:38 +0530

    creating new backrest on rhel8 for pg14 HAPG cluster

---
+++ njani/pgsql-rhel8/backrest/main.tf
@@ -0,0 +1,84 @@
+terraform {
+  backend "s3" {}
+}
+
+locals {
+  etcd_servers    = ["us01vlatetd11","us01vlatetd12","us01vlatetd13"]
+  hapg_servers    = ["us01vlathpg11","us01vlathpg12","us01vlathpg13"]
+  haproxy_server  = ["us01vlathpx11"]
+  backrest_server = ["us01vlatpbk11"]
+  etcd_hosts_p    = ["'us01vlatetd11.auto.saas-n.com','us01vlatetd12.auto.saas-n.com','us01vlatetd13.auto.saas-n.com'"]
+  domain          = "auto.saas-n.com"
+  tier            = "nonprod"
+  bt_env          = "1"
+  bt_product      = "cloud"
+  bt_role         = "postgresql"
+  lob             = "CLOUD"
+  hostgroup       = "BT HA PG Server"
+  environment     = "master"
+  cluster         = "ny5-aza-ntnx-14"
+  network         = "ny2-autolab-app-ahv"
+  datacenter      = "ny2"
+  hapgfacts       = {
+    "bt_env"                  = local.bt_env
+    "bt_tier"                 = local.tier
+    "bt_product"              = local.bt_product
+    "bt_role"                 = local.bt_role
+    "bt_etcd_cluster_members" = ["${local.etcd_servers[0]}.${local.domain}", "${local.etcd_servers[1]}.${local.domain}", "${local.etcd_servers[2]}.${local.domain}"]
+    "bt_hapg_cluster_members" = ["${local.hapg_servers[0]}.${local.domain}", "${local.hapg_servers[1]}.${local.domain}", "${local.hapg_servers[2]}.${local.domain}"]
+    "bt_hapg_node1"           = "${local.hapg_servers[0]}.${local.domain}"
+    "bt_hapg_node2"           = "${local.hapg_servers[1]}.${local.domain}"
+    "bt_hapg_node3"           = "${local.hapg_servers[2]}.${local.domain}"
+    "bt_backup_node"          = "${local.backrest_server[0]}.${local.domain}"
+    "bt_cluster_name"         = "hapgatpg14"
+    "bt_pg_version"           = "14"
+  }
+  haproxyfacts    = {
+    "bt_env"                  = local.bt_env
+    "bt_tier"                 = local.tier
+    "bt_product"              = local.bt_product
+    "bt_role"                 = local.bt_role
+    "bt_etcd_cluster_members" = ["${local.etcd_servers[0]}.${local.domain}", "${local.etcd_servers[1]}.${local.domain}", "${local.etcd_servers[2]}.${local.domain}"]
+    "bt_hapg_cluster_members" = ["${local.hapg_servers[0]}.${local.domain}", "${local.hapg_servers[1]}.${local.domain}", "${local.hapg_servers[2]}.${local.domain}"]
+    "bt_hapg_node1"           = "${local.hapg_servers[0]}.${local.domain}"
+    "bt_hapg_node2"           = "${local.hapg_servers[1]}.${local.domain}"
+    "bt_hapg_node3"           = "${local.hapg_servers[2]}.${local.domain}"
+    "bt_backup_node"          = "${local.backrest_server[0]}.${local.domain}"
+    "bt_cluster_name"         = "hapgatpg14"
+    "bt_pg_version"           = "14"
+  }
+  facts           = {
+    "bt_env"                  = local.bt_env
+    "bt_tier"                 = local.tier
+    "bt_product"              = local.bt_product
+    "bt_etcd_cluster_members" = ["${local.etcd_servers[0]}.${local.domain}", "${local.etcd_servers[1]}.${local.domain}", "${local.etcd_servers[2]}.${local.domain}"]
+  }
+}
+
+
+module "ny2_cdb_backrest_1" {
+  source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
+  hostname             = "${local.backrest_server[0]}"
+  bt_infra_cluster     = local.cluster
+  bt_infra_network     = local.network
+  foreman_hostgroup    = "BT PG Backrest Server"
+  external_facts       = local.facts
+  foreman_environment  = local.environment
+  datacenter           = local.datacenter
+  lob                  = local.lob
+  os_version           = "rhel8"
+  cpus                 = "2"
+  memory               = "4096"
+  additional_disks     = {
+    1 = "100",
+    2 = "160",
+  }
+}
+
+output "ny2_cdb_backrest_1" {
+  value = {
+    "fqdn"  = "module.ny2_cdb_backrest_1.fqdn",
+    "alias" = "module.ny2_cdb_backrest_1.alias",
+    "ip"    = "module.ny2_cdb_backrest_1.ip",
+  }
+}
