4b385bb2004fc940f13b7f51c2a53a23c20f2a5d
Prudhvi.Vuppalapati <Prudhvi.Vuppalapati@bottomline.com>
2023-04-26 15:09:36 +0530

    update vm

--- testz/prudhvi/main.tf
+++ testz/prudhvi/main.tf
@@ -21,8 +21,8 @@ locals {
 
 module "app_server_1" {
   source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
-  hostname             = "us01vlcfrmrd004"
-  alias                = "test-jmxexporterz"
+  hostname             = "us01vlcfrmrd005"
+  alias                = "test-jmxexporterq"
   bt_infra_network     = "ny2-autolab-app-ahv"
   bt_infra_cluster     = "ny5-aza-ntnx-14"
   cpus                 = 6
