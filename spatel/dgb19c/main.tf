7d50054ea75db7ca0a52c8d112caab50280b3784
saumil.patel <saumil.patel@bottomline.com>
2023-05-06 15:34:56 +0530

    oracle19c patch check

--- spatel/dgb19c/main.tf
+++ spatel/dgb19c/main.tf
@@ -5,7 +5,7 @@ terraform {
 locals {
   lob         = "dgb"
   product     = "dgb"
-  environment = "feature_CLOUD_119627"
+  environment = "master"
   datacenter  = "ny2"
   facts         = {
     "bt_customer"         = "fi1234" #ex: fiXXXX
