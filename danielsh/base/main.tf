b3f6e8b2d2fce8481a03da573ccc69db1b8fa2c7
Shine.Daniel <Daniel.Shine@bottomline.com>
2023-06-07 12:39:14 +0300

    add provision

--- danielsh/base/main.tf
+++ danielsh/base/main.tf
@@ -4,17 +4,17 @@ terraform {
  
 locals {
     facts       = {
-      bt_customer         = ""
+      bt_customer         = "base01"
       bt_product          = "cfrmcloud"
       bt_lob              = "CFRM"
       bt_tier             = "dev"
       bt_env              = "master"
       bt_role             = "base"
-      bt_infra_cluster    = "ny5-azg-ntnx-25"
+      bt_infra_cluster    = "ny5-aza-ntnx-14"
       bt_infra_network    = "ny2-autolab-app-ahv"
       hostgroup           = "BT CFRM CLOUD Linux base servers"
       environment         = "master"
-      hostname            = "ny2vlcfrmbs"
+      hostname            = "us01vlcfrmbs"
     }
     datacenter = {
       name = "ny2"
@@ -24,7 +24,7 @@ locals {
  
 module "datahub_1" {
   source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
-  hostname             = "${local.facts.hostname}01" //ny2vlcfrmbs01
+  hostname             = "${local.facts.hostname}" //us01vlcfrmbs
   alias                = "${local.facts.bt_product}-${local.facts.bt_role}-${local.facts.bt_tier}01"//cfrmcloud-base-dev01.saas-n.com
   bt_infra_network     = local.facts.bt_infra_network
   bt_infra_cluster     = local.facts.bt_infra_cluster
