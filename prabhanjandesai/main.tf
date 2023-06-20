b0bce1dbadee86d179131cac02f14ca20cefd486
prabhanjan.desai <Prabhanjan.desai@bottomline.com>
2023-06-20 15:26:17 +0530

    windows_vm

--- prabhanjandesai/main.tf
+++ prabhanjandesai/main.tf
@@ -1,12 +1,6 @@
-terraform {
-  backend "s3" {}
-}
-
-
-
 module "us01vwnxste01" {
   source              = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
-  hostname            = "us01vwnxste04"
+  hostname            = "us01vwnxste09"
   bt_infra_cluster    = "ny2-azb-ntnx-08"
   bt_infra_network    = "ny2-autolab-app-ahv"
   cpus                = 4
@@ -14,15 +8,15 @@ module "us01vwnxste01" {
   memory              = 8192
   os_version          = "win2019"
   foreman_environment = "master"
+  foreman_hostgroup   = "BT Base Server"
   foreman_hostgroup   = "BT Base Windows Server"
-  datacenter          = "ny2"
+  datacenter = "ny2"
 }
-
 
 output "us01vwnxste01" {
   value = {
-    "fqdn"  = module.us01vwnxste01.fqdn,
+    "fqdn" = module.us01vwnxste01.fqdn,
     "alias" = module.us01vwnxste01.alias,
-    "ip"    = module.us01vwnxste01.ip,
+    "ip" = module.us01vwnxste01.ip,
   }
 }
