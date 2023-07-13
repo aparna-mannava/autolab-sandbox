1579b98ee5d72ada9cdf99a0f339fe004745e7ce
Shkelqim <Shkelqim.Aliu@bottomline.com>
2023-07-13 14:42:06 +0200

    fix typo

--- shkelqim/testvm/main.tf
+++ shkelqim/testvm/main.tf
@@ -10,6 +10,7 @@ module "app_server_1" {
   foreman_environment  = "master"
   foreman_hostgroup    = "BT Base Server"
   datacenter           = "ny2"
+  lob                  = "cea" # Replace this with your own line of business
   additional_disks     = {
     1 = "20"
   }
