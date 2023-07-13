251a8dd347129ebe509c27ecdf7d90f491242ff4
Shkelqim <Shkelqim.Aliu@bottomline.com>
2023-07-13 14:27:23 +0200

    test creation of vm for learning

---
+++ shkelqim/testvm/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "s3" {}
+}
+module "app_server_1" {
+  source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
+  hostname             = "us01vltfshkelqim01"
+  bt_infra_cluster     = "ny2-aze-ntnx-11"
+  bt_infra_network     = "ny2-autolab-app-ahv"
+  os_version           = "rhel8"
+  foreman_environment  = "master"
+  foreman_hostgroup    = "BT Base Server"
+  datacenter           = "ny2"
+  lob                  = "YOUR LINE OF BUSINESS" # Replace this with your own line of business
+  additional_disks     = {
+    1 = "20"
+  }
+  external_facts       = {
+    "bt_tier" = "dev"
+  }
+}
+
+output "app_server_1" {
+  value = {
+    "fqdn"  = "${module.app_server_1.fqdn}",
+    "alias" = "${module.app_server_1.alias}",
+    "ip"    = "${module.app_server_1.ip}",
+  }
+}
