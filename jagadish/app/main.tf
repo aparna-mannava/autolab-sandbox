bc0918c0a05c770615b9709a8f6893a5b054d083
Jagadish.Amilineni <Jagadish.Amilineni@BTWIN2VLXBEXyPW.bottomline.tech>
2023-07-13 11:43:53 +0530

    Removed additional facts

--- jagadish/app/main.tf
+++ jagadish/app/main.tf
@@ -15,6 +15,7 @@ locals {
     "bt_groovy_version" = "2.5.8"
     "bt_ic_tag" = "v0007"
     "bt_ic_version" = "6.9.1.SP"
+    "bt_server_number" = "01"
     "bt_infra_cluster" = "ny5-azh-ntnx-26"
     "bt_infra_network" = "ny2-dgb-dgb-preprod-app2"
     "bt_product" = "cfrm"
@@ -32,7 +33,7 @@ locals {
 module "backend_1" {
   source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
   hostname             = local.facts.be01_hostname
-  alias                = "${local.facts.bt_product}-${local.facts.bt_customer}-${local.facts.bt_tier}${local.facts.bt_env}-${local.be1facts.bt_server_mode}${local.be1facts.bt_server_number}-${local.facts.bt_deployment_mode}"
+  alias                = "${local.facts.bt_product}-${local.facts.bt_customer}-${local.facts.bt_tier}${local.facts.bt_env}-${local.facts.bt_server_mode}${local.facts.bt_server_number}-${local.facts.bt_deployment_mode}"
   bt_infra_network     = local.facts.bt_infra_network
   bt_infra_cluster     = local.facts.bt_infra_cluster
   os_version           = local.facts.os_version
@@ -41,29 +42,12 @@ module "backend_1" {
   foreman_environment  = local.environment
   foreman_hostgroup    = local.facts.foreman_hostgroup
   datacenter           = local.datacenter
-  external_facts       = local.be1facts
+  external_facts       = local.facts
   additional_disks     = {
     1 = "50",
     2 = "100"
   }
 }
-
-be1facts    = {
-    "bt_active"   = local.facts.bt_active
-    "bt_customer" = local.facts.bt_customer
-    "bt_product" = local.facts.bt_product
-    "bt_tier" = local.facts.bt_tier
-    "bt_deployment_mode" = local.facts.bt_deployment_mode
-    "bt_env" = local.facts.bt_env
-    "bt_server_mode" = local.facts.bt_server_mode_be
-    "bt_role" = local.facts.bt_role_be
-    "bt_server_number" = "01"
-    "bt_ic_version" = local.facts.bt_ic_version
-    "bt_ic_tag" = local.facts.bt_ic_tag
-    "bt_groovy_version" = local.facts.bt_groovy_version
-    "bt_ach_nutanix_mount" = local.facts.bt_ach_nutanix_mount
-    "bt_alias" = "${local.facts.bt_product}-${local.facts.bt_customer}-${local.facts.bt_tier}${local.facts.bt_env}-${local.facts.bt_server_mode_be}01-${local.facts.bt_deployment_mode}"
-  }
 
 output "backend_1" {
   value = {
