9c0f995319377c381aa6c37cc904ae06a55bb4de
aparna.mannava <aparna.mannava@bottomline.com>
2023-08-02 12:31:00 +0530

    test-server-august

---
+++ mannava/hapg/main.tf
@@ -0,0 +1,159 @@
+terraform {
+  backend "s3" {}
+}
+
+locals {
+  etcd_servers    = ["us01vletcd001"]
+  hapg_servers    = ["us01vlhapgaug001","us01vlhapgaug002","us01vlhapgaug003"]
+  haproxy_server  = ["us01vlprxy001"]
+  backrest_server = ["us01vlbk001"]
+  domain          = "auto.saas-n.com"
+  tier            = "nonprod"
+  bt_env          = "1"
+  bt_product      = "cloud"
+  bt_role         = "postgresql"
+  lob             = "CLOUD"
+  hostgroup       = "BT HA PG Server"
+  environment     = "production"
+  cluster         = "ny5-aza-ntnx-14"
+  network         = "ny2-autolab-app-ahv"
+  datacenter      = "ny2"
+  hapgfacts       = {
+    "bt_env"                  = local.bt_env
+    "bt_tier"                 = local.tier
+    "bt_product"              = local.bt_product 
+    "bt_role"                 = local.bt_role
+    "bt_etcd_cluster_members" = ["${local.etcd_servers[0]}.${local.domain}"]
+    "bt_hapg_cluster_members" = ["${local.hapg_servers[0]}.${local.domain}", "${local.hapg_servers[1]}.${local.domain}", "${local.hapg_servers[2]}.${local.domain}"]
+    "bt_hapg_node1"           = "${local.hapg_servers[0]}.${local.domain}"
+    "bt_hapg_node2"           = "${local.hapg_servers[1]}.${local.domain}"
+	  "bt_hapg_node3"           ="${local.hapg_servers[2]}.${local.domain}"
+    "bt_backup_node"          = "${local.backrest_server[0]}.${local.domain}"
+    "bt_cluster_name"         = "us01vlhapgaug"
+    "bt_pg_version"           = "12"
+  }
+
+  haproxyfacts = {
+    "bt_env"                  = local.bt_env
+    "bt_tier"                 = local.tier
+    "bt_product"              = local.bt_product
+    "bt_role"                 = local.bt_role
+    "bt_etcd_cluster_members" = ["${local.etcd_servers[0]}.${local.domain}"]
+    "bt_hapg_cluster_members" = ["${local.hapg_servers[0]}.${local.domain}", "${local.hapg_servers[1]}.${local.domain}", "${local.hapg_servers[2]}.${local.domain}"]
+    "bt_hapg_node1"           = "${local.hapg_servers[0]}.${local.domain}"
+    "bt_hapg_node2"           = "${local.hapg_servers[1]}.${local.domain}"
+	  "bt_hapg_node3"           = "${local.hapg_servers[2]}.${local.domain}"
+    "bt_backup_node"          = "${local.backrest_server[0]}.${local.domain}"
+    "bt_cluster_name"         = "us01vlhapgaug"
+    "bt_pg_version"           = "12"
+  }
+}
+
+
+module "us01vlhapgaug001" {
+  source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
+  hostname             = "${local.hapg_servers[0]}"
+  bt_infra_cluster     = local.cluster
+  bt_infra_network     = local.network
+  lob                  = local.lob
+  external_facts       = local.hapgfacts
+  foreman_hostgroup    = local.hostgroup
+  foreman_environment  = local.environment
+  datacenter           = local.datacenter
+  os_version           = "rhel8"
+  cpus                 = "2"
+  memory               = "4096"
+  additional_disks     = {
+    1 = "100",
+    2 = "100",
+  }
+}
+
+module "us01vlhapgaug002" {
+  source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
+  hostname             = "${local.hapg_servers[1]}"
+  bt_infra_cluster     = local.cluster
+  bt_infra_network     = local.network
+  foreman_hostgroup    = local.hostgroup
+  foreman_environment  = local.environment
+  datacenter           = local.datacenter
+  lob                  = local.lob
+  os_version           = "rhel8"
+  cpus                 = "2"
+  memory               = "4096"
+  external_facts       = local.hapgfacts
+  additional_disks     = {
+    1 = "100",
+    2 = "100",
+  }
+}
+
+module "us01vlhapgaug003" {
+  source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
+  hostname             = "${local.hapg_servers[2]}"
+  bt_infra_cluster     = local.cluster
+  bt_infra_network     = local.network
+  foreman_hostgroup    = local.hostgroup
+  foreman_environment  = local.environment
+  datacenter           = local.datacenter
+  lob                  = local.lob
+  os_version           = "rhel8"
+  cpus                 = "2"
+  memory               = "4096"
+  external_facts       = local.hapgfacts
+  additional_disks     = {
+    1 = "100",
+    2 = "100",
+  }
+}
+
+module "us01vlprxy001" {
+  source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
+  hostname             = "${local.haproxy_server[0]}"
+  bt_infra_cluster     = local.cluster
+  bt_infra_network     = local.network
+  foreman_hostgroup    = "BT Patroni HA Proxy"
+  foreman_environment  = local.environment
+  os_version           = "rhel8"
+  cpus                 = "2"
+  memory               = "4096"
+  external_facts       = local.haproxyfacts
+  datacenter           = local.datacenter
+  lob                  = local.lob
+  additional_disks     = {
+    1 = "50",
+    2 = "10",
+  }
+}
+
+output "us01vlhapgaug001" {
+  value = {
+    "fqdn"  = "${module.us01vlhapgaug001.fqdn}",
+    "alias" = "${module.us01vlhapgaug001.alias}",
+    "ip"    = "${module.us01vlhapgaug001.ip}",
+  }
+}
+
+output "us01vlhapgaug002" {
+  value = {
+    "fqdn"  = "${module.us01vlhapgaug002.fqdn}",
+    "alias" = "${module.us01vlhapgaug002.alias}",
+    "ip"    = "${module.us01vlhapgaug002.ip}",
+  }
+}
+
+output "us01vlhapgaug003" {
+  value = {
+    "fqdn"  = "${module.us01vlhapgaug003.fqdn}",
+    "alias" = "${module.us01vlhapgaug003.alias}",
+    "ip"    = "${module.us01vlhapgaug003.ip}",
+  }
+}
+
+output "us01vlprxy001" {
+  value = {
+    "fqdn"  = "${module.us01vlprxy001.fqdn}",
+    "alias" = "${module.us01vlprxy001.alias}",
+    "ip"    = "${module.us01vlprxy001.ip}",
+  }
+}
