290f85fd3d338a412405c39bb4d0df896126757c
Nafeesa <nafeesa.yallarthi@bottomline.com>
2023-08-09 22:05:33 +0530

    Rebuild the postgres version 15

---
+++ ny2/postgres/hapg/main.tf
@@ -0,0 +1,129 @@
+terraform {
+  backend "s3" {}
+}
+
+locals {
+  etcd_servers    = ["us01vletcdts50"]
+  hapg_servers    = ["us01vlhapgtest1","us01vlhapgtest2"]
+  haproxy_server  = ["us01vlprxytest1"]
+  backrest_server = ["us01vlbktest1"]
+  domain          = "auto.saas-n.com"
+  tier            = "nonprod"
+  bt_env          = "1"
+  bt_product      = "cloud"
+  bt_role         = "postgresql"
+  lob             = "CLOUD"
+  hostgroup       = "BT HA PG Server"
+  environment     = "feature_CLOUD_000_test_postgres"
+  cluster         = "ny5-aza-ntnx-14"
+  network         = "ny2-autolab-app-ahv"
+  datacenter      = "ny2"
+  hapgfacts       = {
+    "bt_env"                  = local.bt_env
+    "bt_tier"                 = local.tier
+    "bt_product"              = local.bt_product 
+    "bt_role"                 = local.bt_role
+    "bt_etcd_cluster_members" = ["${local.etcd_servers[0]}.${local.domain}"]
+    "bt_hapg_cluster_members" = ["${local.hapg_servers[0]}.${local.domain}", "${local.hapg_servers[1]}.${local.domain}"]
+    "bt_hapg_node1"           = "${local.hapg_servers[0]}.${local.domain}"
+    "bt_hapg_node2"           = "${local.hapg_servers[1]}.${local.domain}"
+    "bt_backup_node"          = "${local.backrest_server[0]}.${local.domain}"
+    "bt_cluster_name"         = "us01vlhapgts70"
+    "bt_pg_version"           = "15"
+  }
+    haproxyfacts = {
+    "bt_env"                  = local.bt_env
+    "bt_tier"                 = local.tier
+    "bt_product"              = local.bt_product
+    "bt_role"                 = local.bt_role
+    "bt_etcd_cluster_members" = ["${local.etcd_servers[0]}.${local.domain}"]
+    "bt_hapg_cluster_members" = ["${local.hapg_servers[0]}.${local.domain}", "${local.hapg_servers[1]}.${local.domain}"]
+    "bt_hapg_node1"           = "${local.hapg_servers[0]}.${local.domain}"
+    "bt_hapg_node2"           = "${local.hapg_servers[1]}.${local.domain}"
+    "bt_backup_node"          = "${local.backrest_server[0]}.${local.domain}"
+    "bt_cluster_name"         = "us01vlhapgts70"
+    "bt_pg_version"           = "15"
+  }
+}
+module "us01vlhapgtest1" {
+  source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
+  hostname             = "${local.hapg_servers[0]}"
+  bt_infra_cluster     = local.cluster
+  bt_infra_network     = local.network
+  lob                  = local.lob
+  external_facts       = local.hapgfacts
+  foreman_hostgroup    = local.hostgroup
+  foreman_environment  = local.environment
+  datacenter           = local.datacenter
+  os_version           = "rhel8"
+  cpus                 = "2"
+  memory               = "4096"
+  additional_disks     = {
+    1 = "100",
+    2 = "100",
+  }
+}
+
+module "us01vlhapgtest2" {
+  source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
+  hostname             = "${local.hapg_servers[1]}"
+  bt_infra_cluster     = local.cluster
+  bt_infra_network     = local.network
+  foreman_hostgroup    = local.hostgroup
+  foreman_environment  = local.environment
+  datacenter           = local.datacenter
+  lob                  = local.lob
+  os_version           = "rhel8"
+  cpus                 = "2"
+  memory               = "4096"
+  external_facts       = local.hapgfacts
+  additional_disks     = {
+    1 = "100",
+    2 = "100",
+  }
+}
+
+module "us01vlprxytest1" {
+  source               = "git::https://gitlab.saas-p.com/shared/terraform-modules/terraform-module-infrastructure.git?ref=master"
+  hostname             = "${local.haproxy_server[0]}"
+  bt_infra_cluster     = local.cluster
+  bt_infra_network     = local.network
+  foreman_hostgroup    = "BT Patroni HA Proxy"
+  foreman_environment  = local.environment
+  os_version           = "rhel8"
+  cpus                 = "2"
+  memory               = "4096"
+  external_facts       = local.haproxyfacts
+  datacenter           = local.datacenter
+  lob                  = local.lob
+  additional_disks     = {
+    1 = "50",
+    2 = "10",
+  }
+}
+
+
+output "us01vlhapgtest1" {
+  value = {
+    "fqdn"  = "${module.us01vlhapgtest1.fqdn}",
+    "alias" = "${module.us01vlhapgtest1.alias}",
+    "ip"    = "${module.us01vlhapgtest1.ip}",
+  }
+}
+
+output "us01vlhapgtest2" {
+  value = {
+    "fqdn"  = "${module.us01vlhapgtest2.fqdn}",
+    "alias" = "${module.us01vlhapgtest2.alias}",
+    "ip"    = "${module.us01vlhapgtest2.ip}",
+  }
+}
+output "us01vlprxytest1" {
+  value = {
+    "fqdn"  = "${module.us01vlprxytest1.fqdn}",
+    "alias" = "${module.us01vlprxytest1.alias}",
+    "ip"    = "${module.us01vlprxytest1.ip}",
+  }
+}
+
+
