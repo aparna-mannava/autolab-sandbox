b248ccca39854fe58206e4b018bc9617ed573dc6
aparna.mannava <aparna.mannava@bottomline.com>
2023-11-19 14:50:27 +0530

    test

--- testupgrade/etcd/main.tf
+++ testupgrade/etcd/main.tf
@@ -13,7 +13,7 @@ locals {
   bt_role         = "postgres"
   hostgroup       = "BT ETCD for PostgreSQL Server"
   environment     = "master"
-  cluster         = "ny5-aza-ntnx-14"
+  cluster         = "ny2-aze-ntnx-14"
   network         = "ny2-autolab-app-ahv"
   datacenter      = "ny2"
   facts           = {
